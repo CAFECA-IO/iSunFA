93652322dc61f52c9cf145f228ae3205ac7643f6
arealclimber <luffulgence@gmail.com>
2024-04-25 14:01:59 +0800

    feat: 🎸 adjust the button and layout on reports sidebar
    ✅ Closes: #346

--- src/components/financial_report_section/financial_report_section.tsx
+++ src/components/financial_report_section/financial_report_section.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '../button/button';
 import DatePicker, { DatePickerType } from '../date_picker/date_picker';
 import { default30DayPeriodInSec } from '../../constants/display';
@@ -72,83 +72,124 @@ const FinancialReportSection = () => {
     setIsLanguageMenuOpen(false);
   };
 
+  useEffect(() => {
+    setPeriod(default30DayPeriodInSec);
+  }, [selectedReportType]);
+
   const selectedLanguage = reportLanguages[selectedReportLanguage];
 
-  const languageMenu = isLanguageMenuOpen ? (
-    <div className="absolute top-[35rem] z-20 max-h-[200px] w-[560px] flex-col overflow-y-auto rounded-sm border border-solid border-slate-300 bg-white pb-2 shadow-dropmenu lg:max-h-[250px]">
-      <div className="flex w-full flex-col pl-2 pt-2">
-        <div className="z-10 flex items-start gap-0">
-          <div className="flex w-full flex-col">
-            {Object.entries(reportLanguages).map(([id, { name, icon }]) => (
-              <button
-                key={id}
-                onClick={() => languageMenuOptionClickHandler(id as ReportLanguages)}
-                type="button"
-                className={`
-                mt-1 flex space-x-3 rounded-sm px-3 py-3 text-navyBlue2 hover:bg-lightGray/10`}
-              >
-                <img src={icon} alt={name} className="mr-2 h-6 w-6" />
-                <p className="justify-center text-base font-medium leading-5 tracking-normal">
-                  {name}
-                </p>
-              </button>
-            ))}
-            {/* {reportLanguages.map((lang) => (
-              <button
-                key={lang.id}
-                onClick={() => languageMenuOptionClickHandler(lang.id)}
-                type="button"
-                className={`
-                mt-1 flex space-x-3 rounded-sm px-3 py-3 text-navyBlue2 hover:bg-lightGray/10`}
-              >
-                <img src={lang.icon} alt={lang.name} className="mr-2 h-6 w-6" />
-                <p className="justify-center text-base font-medium leading-5 tracking-normal">
-                  {lang.name}
-                </p>
-              </button>
-            ))} */}
+  const displayedReportTypeMenu = (
+    <div ref={menuRef} className="relative flex w-full">
+      <button
+        className={`flex w-full items-center justify-between gap-0 rounded-sm border bg-white px-3 py-2.5 ${
+          isMenuOpen ? 'border-input-stroke-selected' : 'border-dropdown-stroke-menu'
+        }`}
+        onClick={menuClickHandler}
+      >
+        <div className="text-base font-medium leading-6 tracking-normal text-input-text-input-filled">
+          {selectedReportName}
+        </div>
+        <div className="my-auto flex flex-col justify-center px-0 py-0">
+          <div className="flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fill="#314362"
+                fillRule="evenodd"
+                d="M4.472 6.97a.75.75 0 011.06 0l4.47 4.47 4.47-4.47a.75.75 0 011.06 1.061l-5 5a.75.75 0 01-1.06 0l-5-5a.75.75 0 010-1.06z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
           </div>
         </div>
+      </button>
+      {/* Info: Report Type Menu (20240425 - Shirley) */}
+      <div
+        className={`absolute left-0 top-[3.5rem] z-20 grid w-full grid-cols-1 overflow-hidden rounded-sm border transition-all duration-300 ease-in-out ${
+          isMenuOpen
+            ? 'grid-rows-1 border-dropdown-stroke-menu shadow-dropmenu'
+            : 'grid-rows-0 border-transparent'
+        }`}
+      >
+        <ul className="z-10 flex w-full flex-col items-start bg-white p-2">
+          {Object.entries(reportTypes).map(([id, { name }]) => (
+            <li
+              key={id}
+              onClick={() => menuOptionClickHandler(id as ReportTypes)}
+              className="mt-1 w-full cursor-pointer px-3 py-2 text-dropdown-text-primary hover:text-text-brand-primary-lv2"
+            >
+              {name}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
-  ) : null;
+  );
 
-  const menu = isMenuOpen ? (
-    <div className="absolute top-[25rem] z-20 max-h-[200px] w-[560px] flex-col overflow-y-auto rounded-sm border border-solid border-slate-300 bg-white pb-2 shadow-dropmenu lg:max-h-[250px]">
-      <div className="flex w-full flex-col pl-2 pt-2">
-        <div className="z-10 flex items-start gap-0">
-          <div className="flex w-full flex-col">
-            {Object.entries(reportTypes).map(([id, { name }]) => (
-              <button
-                key={id}
-                onClick={() => menuOptionClickHandler(id as ReportTypes)}
-                type="button"
-                className={`
-                mt-1 flex rounded-sm px-3 py-3 text-navyBlue2 hover:bg-lightGray/10`}
-              >
-                <p className="justify-center text-base font-medium leading-5 tracking-normal">
-                  {name}
-                </p>
-              </button>
-            ))}
-            {/* {reportTypes.map((type) => (
-              <button
-                key={type.id}
-                onClick={() => menuOptionClickHandler(type.id)}
-                type="button"
-                className={`
-                mt-1 flex rounded-sm px-3 py-3 text-navyBlue2 hover:bg-lightGray/10`}
-              >
-                <p className="justify-center text-base font-medium leading-5 tracking-normal">
-                  {type.name}
-                </p>
-              </button>
-            ))} */}
+  const displayedLanguageMenu = (
+    <div ref={languageMenuRef} className="relative flex w-full">
+      <button
+        className={`flex w-full items-center justify-between gap-0 space-x-5 rounded-sm border bg-white px-3 py-2.5 ${
+          isLanguageMenuOpen ? 'border-input-stroke-selected' : 'border-dropdown-stroke-menu'
+        }`}
+        onClick={languageMenuClickHandler}
+      >
+        <Image
+          width={20}
+          height={20}
+          src={selectedLanguage?.icon ?? '/icons/en.svg'}
+          alt="language icon"
+        />
+        <div className="flex-1 whitespace-nowrap text-start text-base font-medium leading-6 tracking-normal text-slate-700">
+          {selectedLanguage?.name}
+        </div>
+        <div className="my-auto flex flex-col justify-center px-0 py-0">
+          <div className="flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fill="#314362"
+                fillRule="evenodd"
+                d="M4.472 6.97a.75.75 0 011.06 0l4.47 4.47 4.47-4.47a.75.75 0 011.06 1.061l-5 5a.75.75 0 01-1.06 0l-5-5a.75.75 0 010-1.06z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
           </div>
         </div>
+      </button>
+      {/* Info: Language Menu (20240425 - Shirley) */}
+      <div
+        className={`absolute left-0 top-[3.5rem] z-20 grid w-full grid-cols-1 overflow-hidden rounded-sm border transition-all duration-300 ease-in-out ${
+          isLanguageMenuOpen
+            ? 'grid-rows-1 border-dropdown-stroke-menu shadow-dropmenu'
+            : 'grid-rows-0 border-transparent'
+        }`}
+      >
+        <ul className="z-10 flex w-full flex-col items-start bg-white p-2">
+          {Object.entries(reportLanguages).map(([id, { name, icon }]) => (
+            <li
+              key={id}
+              onClick={() => languageMenuOptionClickHandler(id as ReportLanguages)}
+              className="mt-1 flex w-full cursor-pointer items-center space-x-5 px-1 py-2.5 text-navyBlue2 hover:text-text-brand-primary-lv2"
+            >
+              <img src={icon} alt={name} className="h-6 w-6" />
+              <p className="text-base font-medium leading-5 tracking-normal">{name}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
-  ) : null;
+  );
 
   return (
     <div className="mt-20 flex w-full shrink-0 grow basis-0 flex-col bg-surface-neutral-main-background px-0 pb-0">
@@ -170,39 +211,7 @@ const FinancialReportSection = () => {
             <div className="justify-center text-sm font-semibold leading-5 tracking-normal text-slate-700 max-md:max-w-full">
               Report Type
             </div>
-            <div className="flex w-full" ref={menuRef}>
-              <button
-                className={`${
-                  isMenuOpen ? 'border-primaryYellow' : 'border-slate-300'
-                } flex w-full gap-0 rounded-sm border border-solid bg-white max-md:flex-wrap`}
-                onClick={menuClickHandler}
-              >
-                <div className="flex flex-1 flex-col justify-center text-base font-medium leading-6 tracking-normal text-slate-700 max-md:max-w-full">
-                  <div className="items-start px-3 py-2.5 text-start max-md:max-w-full max-md:pr-5">
-                    {selectedReportName}
-                  </div>
-                </div>
-                <div className="my-auto flex flex-col justify-center px-3 py-2.5">
-                  <div className="flex items-center justify-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      fill="none"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        fill="#314362"
-                        fillRule="evenodd"
-                        d="M4.472 6.97a.75.75 0 011.06 0l4.47 4.47 4.47-4.47a.75.75 0 011.06 1.061l-5 5a.75.75 0 01-1.06 0l-5-5a.75.75 0 010-1.06z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                  </div>
-                </div>
-              </button>
-              {menu}
-            </div>
+            {displayedReportTypeMenu}
           </div>
         </div>
         <div className="mt-20 flex flex-col justify-center max-md:mt-10 max-md:max-w-full">
@@ -210,47 +219,7 @@ const FinancialReportSection = () => {
             <div className="justify-center text-sm font-semibold leading-5 tracking-normal text-slate-700 max-md:max-w-full">
               Report Language
             </div>
-            <div ref={languageMenuRef} className="flex w-full">
-              <button
-                className={`${
-                  isLanguageMenuOpen ? 'border-primaryYellow' : 'border-slate-300'
-                } flex w-full gap-0 rounded-sm border border-solid bg-white max-md:flex-wrap`}
-                onClick={languageMenuClickHandler}
-              >
-                <div className="my-auto flex flex-col justify-center self-stretch px-3 py-2.5">
-                  <Image
-                    width={20}
-                    height={20}
-                    src={selectedLanguage?.icon ?? '/icons/en.svg'}
-                    alt="language icon"
-                  />
-                </div>
-                <div className="flex flex-1 flex-col justify-start self-stretch whitespace-nowrap text-start text-base font-medium leading-6 tracking-normal text-slate-700 max-md:max-w-full">
-                  <div className="items-start justify-start px-3 py-2.5 text-start max-md:max-w-full max-md:pr-5">
-                    {selectedLanguage?.name}
-                  </div>
-                </div>
-                <div className="my-auto flex flex-col justify-end self-stretch px-3 py-2.5">
-                  <div className="flex items-center justify-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      fill="none"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        fill="#314362"
-                        fillRule="evenodd"
-                        d="M4.472 6.97a.75.75 0 011.06 0l4.47 4.47 4.47-4.47a.75.75 0 011.06 1.061l-5 5a.75.75 0 01-1.06 0l-5-5a.75.75 0 010-1.06z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                  </div>
-                </div>
-              </button>
-              {languageMenu}
-            </div>
+            {displayedLanguageMenu}
           </div>
         </div>
         <div className="mt-20 flex flex-col max-md:mt-10 max-md:max-w-full">
