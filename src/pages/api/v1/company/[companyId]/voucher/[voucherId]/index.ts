c318f4b9fd0709e0f4d9e9333ea312f17081f936
RexBearIU <rexbear87941@gmail.com>
2024-05-03 16:14:33 +0800

    feat: demo part mock api

---
+++ src/pages/api/v1/company/[companyId]/voucher/[voucherId]/index.ts
@@ -0,0 +1,81 @@
+import { IResponseData } from '@/interfaces/response_data';
+import { IVoucher } from '@/interfaces/voucher';
+import { errorMessageToErrorCode } from '@/lib/utils/errorCode';
+import version from '@/lib/version';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IResponseData<IVoucher>>
+) {
+  try {
+    if (req.method === 'GET') {
+      if (!req.query.voucherId) {
+        throw new Error('Invalid input parameter');
+      }
+      const voucher: IVoucher = {
+        voucherIndex: '1',
+        metadatas: [
+          {
+            date: 1713139200000,
+            voucherType: 'expense',
+            companyId: '1',
+            companyName: '文中資訊股份有限公司',
+            description:
+              'WSTP會計師工作輔助幫手: 88725, 文中網路版主機授權費用: 8400, 文中工作站授權費用: 6300',
+            totalPrice: 109725,
+            taxPercentage: 5,
+            fee: 0,
+            paymentMethod: 'transfer',
+            paymentPeriod: 'atOnce',
+            installmentPeriod: 0,
+            paymentStatus: 'unpaid',
+            alreadyPaidAmount: 0,
+          },
+        ],
+        lineItems: [
+          {
+            lineItemIndex: '2',
+            account: '124124',
+            description: 'WSTP會計師工作輔助幫手',
+            debit: false,
+            amount: 5,
+          },
+          {
+            lineItemIndex: '3',
+            account: '124124',
+            description: '文中網路版主機授權費用',
+            debit: false,
+            amount: 8400,
+          },
+          {
+            lineItemIndex: '4',
+            account: '124124',
+            description: '文中工作站授權費用',
+            debit: false,
+            amount: 6300,
+          },
+        ],
+      };
+      res.status(200).json({
+        powerby: 'ISunFa api ' + version,
+        success: true,
+        code: '200',
+        message: 'get voucher by id',
+        payload: voucher,
+      });
+    } else {
+      throw new Error('Method Not Allowed');
+    }
+  } catch (_error) {
+    const error = _error as Error;
+    const statusCode = errorMessageToErrorCode(error.message);
+    res.status(statusCode).json({
+      powerby: 'ISunFa api ' + version,
+      success: false,
+      code: String(statusCode),
+      payload: {},
+      message: error.message,
+    });
+  }
+}
