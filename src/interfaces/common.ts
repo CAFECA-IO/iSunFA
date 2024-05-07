f18cbb4bc384c4733b51171b079be4a39ae2ea43
Tiny_Murky <murky0830@gmail.com>
2024-05-07 17:05:48 +0800

    migrate ocr to invoice

--- src/interfaces/common.ts
+++ src/interfaces/common.ts
@@ -1,3 +1,4 @@
+export type ProgressStatus = 'success' | 'inProgress' | 'error' | 'notFound';
 // Info Murky (20240505): type guards can input any type and return a boolean
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isStringNumber(value: any): value is string {
