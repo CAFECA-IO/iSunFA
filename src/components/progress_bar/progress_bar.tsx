aa65c4348d136ffe7458425bf64fb669099f8350
julian <julian890701@gmail.com>
2024-05-09 12:02:07 +0800

    feat: progress bar style done

--- src/components/progress_bar/progress_bar.tsx
+++ src/components/progress_bar/progress_bar.tsx
@@ -1,15 +1,44 @@
+import { useEffect } from 'react';
+
 interface IProgressBarProps {
   progressRate: number;
   progressRateChangeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const ProgressBar = ({ progressRate, progressRateChangeHandler }: IProgressBarProps) => {
+  useEffect(() => {
+    // Info: (20240509 - Julian) 找到 sliderProgress 的 style element
+    const styleElement = document.getElementById('sliderStyle');
+
+    // Info: (20240509 - Julian) 如果有找到 style element，則更新 style element 的內容
+    if (styleElement) {
+      // Info: (20240509 - Julian) 將進度條的背景色設定為線性漸層，並根據進度比例設定顏色
+      styleElement.innerHTML = `
+        .sliderProgress::-webkit-slider-runnable-track {
+          background: linear-gradient(90deg, #FFA502 ${progressRate}%, #cdd1d9 ${progressRate}%);
+        }
+      `;
+    } else {
+      // Info: (20240509 - Julian) 如果沒有找到 style element，則建立一個新的 style element
+      const newStyleElement = document.createElement('style');
+      newStyleElement.id = 'sliderStyle';
+      newStyleElement.innerHTML = `
+        .sliderProgress::-webkit-slider-runnable-track {
+          background: linear-gradient(90deg, #FFA502 ${progressRate}%, #cdd1d9 ${progressRate}%);
+        }
+      `;
+      // Info: (20240509 - Julian) 將新的 style element 加入到 document head
+      document.head.appendChild(newStyleElement);
+    }
+  }, [progressRate]);
+
   return (
     <div className="flex w-full flex-col items-start gap-8px">
       <p className="text-sm font-semibold text-navyBlue2">Progress</p>
       <div className="flex w-full flex-col gap-x-20px gap-y-10px md:flex-row">
         {/* Info: (20240502 - Julian) Progress Bar */}
         <input
+          id="sliderProgress"
           type="range"
           min={0}
           max={100}
@@ -32,6 +61,7 @@ const ProgressBar = ({ progressRate, progressRateChangeHandler }: IProgressBarPr
             max={100}
             className="flex-1 bg-transparent px-10px outline-none"
           />
+
           <div className="flex items-center gap-4px p-12px text-sm text-lightGray4">
             <p>%</p>
           </div>
