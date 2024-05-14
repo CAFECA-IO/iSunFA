70944bb621efaed362b342ed56a10f0fc71333a5
Tiny_Murky <murky0830@gmail.com>
2024-05-14 13:38:05 +0800

    report-analysis, reportid

--- src/pages/api/v1/company/[companyId]/report_analysis/[reportId]/index.ts
+++ src/pages/api/v1/company/[companyId]/report_analysis/[reportId]/index.ts
@@ -1,10 +1,9 @@
 import { AICH_URI } from '@/constants/config';
 import { IResponseData } from '@/interfaces/response_data';
-import { errorMessageToErrorCode } from '@/lib/utils/error_code';
-// import { RESPONSE_STATUS_MESSAGE } from '@/constants/STATUS_MESSAGE';
-import version from '@/lib/version';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { IFinancialReportJSON, isIFinancialReportJSON } from '@/interfaces/financial_report';
+import { formatApiResponse } from '@/lib/utils/common';
+import { STATUS_MESSAGE } from '@/constants/status_code';
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,47 +15,35 @@ export default async function handler(
         throw new Error('INVALID_INPUT_PARAMETER');
       }
 
-      const result = await fetch(`${AICH_URI}/api/v1/audit_reports/${req.query.reportId}/result`);
+      const fetchResult = await fetch(
+        `${AICH_URI}/api/v1/audit_reports/${req.query.reportId}/result`
+      );
 
-      if (!result.ok) {
-        throw new Error('GATEWAY_TIMEOUT');
+      if (!fetchResult.ok) {
+        throw new Error(STATUS_MESSAGE.BAD_GATEWAY_AICH_FAILED);
       }
 
-      const financialReportJSON = (await result.json()).payload;
-      // ToDo: (20240514 - Jacky) Should use uniform response handler
-      if (!financialReportJSON) {
-        res.status(200).json({
-          powerby: 'ISunFa api ' + version,
-          success: false,
-          code: String(200),
-          message: 'get Financial JSON result by id',
-          payload: financialReportJSON,
-        });
-      }
+      const financialReportJSON = (await fetchResult.json()).payload;
 
       if (!isIFinancialReportJSON(financialReportJSON)) {
-        throw new Error('INTERNAL_SERVICE_ERROR');
+        throw new Error(STATUS_MESSAGE.BAD_GATEWAY_DATA_FROM_AICH_IS_INVALID_TYPE);
       }
-      // ToDo: (20240514 - Jacky) Should use uniform response handler
-      res.status(200).json({
-        powerby: 'ISunFa api ' + version,
-        success: true,
-        code: String(200),
-        message: 'get Financial JSON result by id',
-        payload: financialReportJSON,
-      });
+
+      const resultJson: IFinancialReportJSON = (await fetchResult.json()).payload;
+      const { httpCode, result } = formatApiResponse<IFinancialReportJSON>(
+        STATUS_MESSAGE.SUCCESS_GET,
+        resultJson
+      );
+      res.status(httpCode).json(result);
     } else {
       throw new Error('METHOD_NOT_ALLOWED');
     }
   } catch (_error) {
     const error = _error as Error;
-    const statusCode = errorMessageToErrorCode(error.message);
-    res.status(statusCode).json({
-      powerby: 'ISunFa api ' + version,
-      success: false,
-      code: String(statusCode),
-      payload: {},
-      message: error.message,
-    });
+    const { httpCode, result } = formatApiResponse<IFinancialReportJSON>(
+      error.message,
+      {} as IFinancialReportJSON
+    );
+    res.status(httpCode).json(result);
   }
 }
