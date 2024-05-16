b130d1af68fdeb3ab4abf592ed919ee6e8239904
Luphia Chang <luphia@cafeca.io>
2024-05-16 16:48:45 +0800

    Fix interfaces

--- src/interfaces/invoice.ts
+++ src/interfaces/invoice.ts
@@ -1,4 +1,4 @@
-import { EventType, isEventType } from '@/interfaces/account';
+import { EventType, isEventType } from '@/constants/account';
 import { IPayment, isIPayment } from '@/interfaces/payment';
 
 export interface IInvoice {
