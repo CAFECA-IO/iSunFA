95b61ff8729ba378161b41bf53548153aeb2afba
arealclimber <luffulgence@gmail.com>
2024-05-21 17:37:05 +0800

    feat: 🎸 fine-tune the data displayed on income-expense chart
    on dashboard page
    
    ✅ Closes: #786

--- src/components/income_expense_trend_chart/income_expense_trend_chart.tsx
+++ src/components/income_expense_trend_chart/income_expense_trend_chart.tsx
@@ -175,14 +175,11 @@ const LineChart = ({ data }: LineChartProps) => {
         // formatter: value => `${value}`,
       },
       y: {
-        // formatter: (value) => `${value}%`,
         formatter: (value, { series, seriesIndex, dataPointIndex }) => {
-          console.log('in y formatter', value, series, seriesIndex, dataPointIndex);
-          // const absoluteValue = series[seriesIndex][dataPointIndex];
           const absoluteValue = data.annotations[seriesIndex].data[dataPointIndex].absolute;
           const formattedAbsoluteValue = absoluteValue.toLocaleString(); // 使用 toLocaleString() 方法加上千分位逗號
 
-          return `${value}%, ${formattedAbsoluteValue}`;
+          return `${formattedAbsoluteValue}　(${value}%)`;
         },
       },
       marker: {
