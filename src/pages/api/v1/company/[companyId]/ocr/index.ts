48391f098406e3e530a1cd648fd1bb28919f9d57
Tiny_Murky <murky0830@gmail.com>
2024-05-21 11:49:51 +0800

    init

---
+++ src/pages/api/v1/company/[companyId]/ocr/index.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { IResponseData } from '@/interfaces/response_data';
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IResponseData<null>>
+) {
+
+}
