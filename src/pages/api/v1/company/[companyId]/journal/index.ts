69b749a7804166afd2592e507fa096185152578a
Tiny_Murky <murky0830@gmail.com>
2024-05-24 19:33:43 +0800

    get all journal done

--- src/pages/api/v1/company/[companyId]/journal/index.ts
+++ src/pages/api/v1/company/[companyId]/journal/index.ts
@@ -1,19 +1,62 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { IJournal } from '@/interfaces/journal';
 import { IResponseData } from '@/interfaces/response_data';
-import { formatApiResponse, pageToOffset, timestampInMilliSeconds } from '@/lib/utils/common';
+import { formatApiResponse, pageToOffset, timestampInMilliSeconds, timestampInSeconds } from '@/lib/utils/common';
 
 import { STATUS_MESSAGE } from '@/constants/status_code';
 import prisma from '@/client';
 import { DEFAULT_PAGE_LIMIT, DEFAULT_PAGE_START_AT } from '@/constants/config';
-import { EventType } from '@/constants/account';
+
+type ApiResponseType = {
+  id: number;
+  date: number;
+  type: string | undefined;
+  particulars: string | undefined;
+  fromTo: string | undefined;
+  account: {
+    id:number;
+    debit: boolean;
+    account: string;
+    amount: number;
+  }[] | undefined;
+  projectName: string | undefined;
+  projectImageId: string | null | undefined;
+  voucherId: number | undefined;
+  voucherNo: string | undefined;
+}[];
+
+type PrismaReturnType = {
+  id: number;
+  createdAt: Date;
+  invoice: {
+    eventType: string;
+    description: string;
+    vendorOrSupplier: string;
+  } | null;
+  project: {
+    name: string;
+    id: number;
+    imageId: string | null;
+  } | null;
+  voucher: {
+    id: number;
+    no: string;
+    lineItems: {
+      id: number;
+      amount: number;
+      debit: boolean;
+      account: {
+        name: string;
+      };
+    }[];
+  } | null
+}[];
 
 async function getJournal(
   companyId: number,
   page: number = DEFAULT_PAGE_START_AT,
   limit: number = DEFAULT_PAGE_LIMIT,
-  eventType: EventType | undefined = undefined,
+  eventType: string | undefined = undefined,
   startDate: number | undefined = undefined,
   endDate: number | undefined = undefined,
   search: string | undefined = undefined,
@@ -21,11 +64,13 @@ async function getJournal(
 ) {
   const startDateInMilliSecond = startDate ? new Date(timestampInMilliSeconds(startDate)) : undefined;
   const endDateInMilliSecond = endDate ? new Date(timestampInMilliSeconds(endDate)) : undefined;
-  const eventTypeInString = eventType ? EventType[eventType] : undefined;
 
   const offset = pageToOffset(page, limit);
   try {
     const journalData = await prisma.journal.findMany({
+      orderBy: {
+        createdAt: sort === 'asc' ? 'asc' : 'desc'
+      },
       skip: offset,
       take: limit,
       where: {
@@ -35,7 +80,7 @@ async function getJournal(
           lte: endDateInMilliSecond
         },
         invoice: {
-          eventType: eventTypeInString,
+          eventType,
         },
         OR: [
           {
@@ -68,6 +113,7 @@ async function getJournal(
           select: {
             id: true,
             name: true,
+            imageId: true,
           }
         },
         invoice: {
@@ -103,23 +149,76 @@ async function getJournal(
   }
 }
 
-function formatJournal(journalData: ) {
-
+function formatJournal(journalData: PrismaReturnType) {
+  const journals = journalData.map((journal) => {
+    return {
+      id: journal.id,
+      date: timestampInSeconds(journal.createdAt.getTime()),
+      type: journal.invoice?.eventType,
+      particulars: journal.invoice?.description,
+      fromTo: journal.invoice?.vendorOrSupplier,
+      account: journal.voucher?.lineItems.map((lineItem) => {
+        return {
+          id: lineItem.id,
+          debit: lineItem.debit,
+          account: lineItem.account.name,
+          amount: lineItem.amount,
+        };
+      }),
+      projectName: journal.project?.name,
+      projectImageId: journal.project?.imageId,
+      voucherId: journal.voucher?.id,
+      voucherNo: journal.voucher?.no,
+    };
+  });
+  return journals;
 }
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<IResponseData<IJournal | IJournal[]>>
+  res: NextApiResponse<IResponseData<ApiResponseType>>
 ) {
   try {
     if (req.method === 'GET') {
-      if (!journalArray) {
-        throw new Error(STATUS_MESSAGE.RESOURCE_NOT_FOUND);
+      const {
+        companyId,
+        page, // can be undefined
+        limit,
+        eventType,
+        startDate,
+        endDate,
+        search,
+        sort
+      } = req.query;
+      // help me check type
+
+      if (
+         Array.isArray(companyId) ||
+        !companyId ||
+        typeof companyId !== 'string' ||
+        !Number.isInteger(Number(companyId)) ||
+        (page && !Number.isInteger(Number(page))) ||
+        (limit && !Number.isInteger(Number(limit))) ||
+        (eventType && typeof eventType !== 'string') ||
+        (startDate && !Number.isInteger(Number(startDate))) ||
+        (endDate && !Number.isInteger(Number(endDate))) ||
+        (search && typeof search !== 'string') ||
+        (sort && typeof sort !== 'string')) {
+        throw new Error(STATUS_MESSAGE.INVALID_INPUT_PARAMETER);
       }
 
-      const { httpCode, result } = formatApiResponse<IJournal[]>(
+      const companyIdNumber = Number(companyId);
+      const pageInt = page ? Number(page) : DEFAULT_PAGE_START_AT;
+      const limitInt = limit ? Number(limit) : DEFAULT_PAGE_LIMIT;
+      const startDateInt = startDate ? Number(startDate) : undefined;
+      const endDateInt = endDate ? Number(endDate) : undefined;
+
+      const journalData = await getJournal(companyIdNumber, pageInt, limitInt, eventType, startDateInt, endDateInt, search, sort);
+      const journals = formatJournal(journalData);
+
+      const { httpCode, result } = formatApiResponse<ApiResponseType>(
         STATUS_MESSAGE.SUCCESS_LIST,
-        journalArray
+        journals
       );
 
       res.status(httpCode).json(result);
@@ -128,7 +227,7 @@ export default async function handler(
     }
   } catch (_error) {
     const error = _error as Error;
-    const { httpCode, result } = formatApiResponse<IJournal>(error.message, {} as IJournal);
+    const { httpCode, result } = formatApiResponse<ApiResponseType>(error.message, {} as ApiResponseType);
     res.status(httpCode).json(result);
   }
 }
