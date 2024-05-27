7088effe54c6963679d2864d2ceaeadac617f9c5
Tiny_Murky <murky0830@gmail.com>
2024-05-27 19:31:58 +0800

    delete comment

--- src/interfaces/journal.ts
+++ src/interfaces/journal.ts
@@ -22,58 +22,6 @@ export interface IJournalData {
   voucher: IVoucherDataForSavingToDB | null
 }
 
-// export interface IJournalData {
-//   id: number;
-//   tokenContract: string | null;
-//   tokenId: string | null;
-//   aichResultId: string | null;
-//   projectId: number | null;
-//   contractId: number | null;
-//   ocr: {
-//     id: number;
-//     imageName: string;
-//     imageUrl: string;
-//     imageSize: number;
-//     createdAt: Date;
-//     updatedAt: Date;
-//   } | null; // Info: 跟 IUnprocessedJournal 是否可以通用 @Murky (20240527 - tzuhan)
-//   invoice: {
-//     // Info: 跟 IInvoiceDataForSavingToDB 是否可以通用 @Murky (20240527 - tzuhan)
-//     id: number;
-//     eventType: string;
-//     description: string;
-//     vendorOrSupplier: string;
-//     payment: {
-//       id: number;
-//       isRevenue: boolean;
-//       price: number;
-//       hasTax: boolean;
-//       taxPercentage: number;
-//       hasFee: boolean;
-//       fee: number;
-//       paymentMethod: string;
-//       paymentPeriod: string;
-//       installmentPeriod: number;
-//       paymentAlreadyDone: number;
-//       paymentStatus: string;
-//       progress: number;
-//     };
-//   } | null;
-//   voucher: {
-//     id: number;
-//     no: string;
-//     lineItems: ILineItem[];
-//     // Info: 跟 ILineItem 是否可以通用 @Murky (20240527 - tzuhan)
-//     // {
-//     //   id: number;
-//     //   amount: number;
-//     //   debit: boolean;
-//     //   account: {
-//     //     name: string;
-//     //   };
-//     // }[];
-//   } | null;
-// }
 export interface IUnprocessedJournal {
   id: number;
   aichResultId: string;
