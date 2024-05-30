5e9809c5dce473f5d02d8a744f589e5f1a7d753d
gibbs.shih <gibbs.shih@isuncloud.com>
2024-05-30 10:30:53 +0800

    update db schema

--- src/pages/api/v1/company/[companyId]/labor_cost_chart/actual.ts
+++ src/pages/api/v1/company/[companyId]/labor_cost_chart/actual.ts
@@ -1,9 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { ILaborCostChartData, DUMMY_LABOR_COST_CHART_DATA } from '@/interfaces/labor_cost_chart';
+import { ILaborCostChartData } from '@/interfaces/labor_cost_chart';
 import { IResponseData } from '@/interfaces/response_data';
 import { STATUS_MESSAGE } from '@/constants/status_code';
 import { formatApiResponse } from '@/lib/utils/common';
-import prisma from '@/client';
+// import prisma from '@/client';
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,12 +13,12 @@ export default async function handler(
     if (req.method !== 'GET') {
       throw new Error(STATUS_MESSAGE.METHOD_NOT_ALLOWED);
     }
+    // (Todo: use date to get data from db)
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { date } = req.query;
-    console.log("h111111i");
     // get workrate all data from db
-    const data = await prisma.workRate.findMany();
-    console.log(data);
-    res.status(httpCode).json(result);
+    // const data = await prisma.workRate.findMany();
+    // res.status(httpCode).json(result);
   } catch (_error) {
     const error = _error as Error;
     const { httpCode, result } = formatApiResponse<ILaborCostChartData>(
