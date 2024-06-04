ac31e2f5b38a160904756417368728657ba8684c
Tiny_Murky <murky0830@gmail.com>
2024-06-04 18:03:42 +0800

    temp

--- prisma/prisma_mock.ts
+++ prisma/prisma_mock.ts
@@ -1,16 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
 
-import prisma from './client';
+// import prisma from './client';
 
-jest.mock('./client', () => ({
-  __esModule: true,
-  default: mockDeep<PrismaClient>(),
-}));
+// jest.mock('./client', () => ({
+//   __esModule: true,
+//   default: mockDeep<PrismaClient>(),
+// }));
 
-beforeEach(() => {
-  // Info: (20240524 Murky) prisma 官方推薦寫法 https://www.prisma.io/docs/orm/prisma-client/testing/unit-testing
-  // eslint-disable-next-line @typescript-eslint/no-use-before-define
-  mockReset(prismaMock);
-});
-export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
+// beforeEach(() => {
+//   // Info: (20240524 Murky) prisma 官方推薦寫法 https://www.prisma.io/docs/orm/prisma-client/testing/unit-testing
+//   // eslint-disable-next-line @typescript-eslint/no-use-before-define
+//   mockReset(prismaMock);
+// });
+// export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
