daf50366e7a9766655708c646bf85291c56a2171
Tiny_Murky <murky0830@gmail.com>
2024-06-04 10:32:28 +0800

    google init

---
+++ src/constants/google.ts
@@ -0,0 +1,5 @@
+export const GOOGLE_PROJECT_ID = process.env.GOOGLE_PROJECT_ID || '';
+export const GOOGLE_SERVICE_KEY = process.env.GOOGLE_SERVICE_KEY || '';
+export const GOOGLE_STORAGE_BUCKET_NAME = process.env.GOOGLE_STORAGE_BUCKET_NAME || '';
+export const GOOGLE_UPLOAD_FOLDER = process.env.GOOGLE_UPLOAD_FOLDER || '';
+export const GOOGLE_STORAGE_BUCKET_URL = `https://storage.googleapis.com/${GOOGLE_STORAGE_BUCKET_NAME}}`;
