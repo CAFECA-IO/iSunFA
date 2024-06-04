da4a294f7c14d1a225256f1da9cd113d1062a22f
Tiny_Murky <murky0830@gmail.com>
2024-06-04 17:25:37 +0800

    prisma update

--- src/constants/google.ts
+++ src/constants/google.ts
@@ -1,6 +1,6 @@
-export const GOOGLE_PROJECT_ID = process.env.GOOGLE_PROJECT_ID || '';
-export const GOOGLE_STORAGE_BUCKET_NAME = process.env.GOOGLE_BUCKET_NAME || '';
-export const GOOGLE_UPLOAD_FOLDER = process.env.GOOGLE_UPLOAD_FOLDER || '';
+export const GOOGLE_PROJECT_ID = process.env.GOOGLE_PROJECT_ID || 'mermer-official-415806';
+export const GOOGLE_STORAGE_BUCKET_NAME = process.env.GOOGLE_BUCKET_NAME || 'mermer-offical-km';
+export const GOOGLE_UPLOAD_FOLDER = process.env.GOOGLE_UPLOAD_FOLDER || 'ISunFa';
 export const GOOGLE_STORAGE_BUCKET_URL = `https://storage.googleapis.com/${GOOGLE_STORAGE_BUCKET_NAME ? GOOGLE_STORAGE_BUCKET_NAME + '/' : ''}`;
 export const GOOGLE_CREDENTIALS_BASE64 = process.env.GOOGLE_CREDENTIALS_BASE64 || '';
 export const GOOGLE_CREDENTIALS = JSON.parse(
