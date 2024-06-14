ee1117969601465456bd9d4b1e2006805e8b673d
gibbs.shih <gibbs.shih@isuncloud.com>
2024-06-14 15:08:48 +0800

    update test file

--- src/pages/api/v1/company/[companyId]/report_pending/index.test.ts
+++ src/pages/api/v1/company/[companyId]/report_pending/index.test.ts
@@ -1,79 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { ReportKind } from '@/interfaces/report_item';
-import { FinancialReportTypesKey } from '@/interfaces/report_type';
-import prisma from '@/client';
 import handler from './index';
 
 let req: jest.Mocked<NextApiRequest>;
 let res: jest.Mocked<NextApiResponse>;
-
-const dummyPendingReportItems = [
-  {
-    id: 8888871,
-    name: 'Cash Flow Statement-20240420-1',
-    from: 1683043200,
-    to: 1704067200,
-    remainingSeconds: 10,
-    paused: false,
-    reportType: FinancialReportTypesKey.cash_flow_statement,
-    type: ReportKind.financial,
-    status: 'pending',
-    createdAt: 1713611226,
-    updatedAt: 1713611226,
-  },
-  {
-    id: 8888872,
-    name: 'Cash Flow Statement-20240505-1',
-    from: 1695609600,
-    to: 1698106883,
-    remainingSeconds: 250,
-    paused: true,
-    reportType: FinancialReportTypesKey.cash_flow_statement,
-    type: ReportKind.financial,
-    status: 'pending',
-    createdAt: 1714897574,
-    updatedAt: 1714897574,
-  },
-  {
-    id: 8888873,
-    name: 'Comprehensive Income Statement-20240412-1',
-    remainingSeconds: 1615,
-    paused: false,
-    reportType: FinancialReportTypesKey.comprehensive_income_statement,
-    type: ReportKind.financial,
-    from: 1685721600,
-    to: 1704076800,
-    status: 'pending',
-    createdAt: 1712863312,
-    updatedAt: 1712863312,
-  },
-  {
-    id: 8888874,
-    name: 'Balance Sheet-20240423-1',
-    remainingSeconds: 3680,
-    paused: false,
-    reportType: FinancialReportTypesKey.balance_sheet,
-    type: ReportKind.financial,
-    from: 1693113600,
-    to: 1704096000,
-    status: 'pending',
-    createdAt: 1713846643,
-    updatedAt: 1713846643,
-  },
-  {
-    id: 8888875,
-    name: 'Balance Sheet-20240501-1',
-    remainingSeconds: 30,
-    paused: false,
-    reportType: FinancialReportTypesKey.balance_sheet,
-    type: ReportKind.financial,
-    from: 1698374400,
-    to: 1714022400,
-    status: 'pending',
-    createdAt: 1714508675,
-    updatedAt: 1714508675,
-  },
-];
 
 beforeEach(async () => {
   req = {
@@ -96,9 +25,6 @@ afterEach(async () => {
 
 describe('pendingReports API Handler Tests', () => {
   it('should handle GET requests successfully', async () => {
-    await prisma.report.createMany({
-      data: dummyPendingReportItems,
-    });
     req.method = 'GET';
     await handler(req, res);
     expect(res.status).toHaveBeenCalledWith(200);
@@ -125,13 +51,6 @@ describe('pendingReports API Handler Tests', () => {
         payload: expect.arrayContaining([expectedStructure]),
       })
     );
-    await prisma.report.deleteMany({
-      where: {
-        id: {
-          in: dummyPendingReportItems.map((item) => item.id),
-        },
-      },
-    });
   });
   it('should return [] if no data', async () => {
     req.method = 'GET';
