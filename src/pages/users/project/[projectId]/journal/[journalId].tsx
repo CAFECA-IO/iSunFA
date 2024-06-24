fe3dd9ba8802762000915253ef69ace6cf9b644a
julian <julian890701@gmail.com>
2024-06-24 15:53:18 +0800

    Feat: project journal detail page done

--- src/pages/users/project/[projectId]/journal/[journalId].tsx
+++ src/pages/users/project/[projectId]/journal/[journalId].tsx
@@ -1,23 +1,24 @@
 import Head from 'next/head';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { GetServerSideProps } from 'next';
 import NavBar from '@/components/nav_bar/nav_bar';
-import AccountingSidebar from '@/components/accounting_sidebar/accounting_sidebar';
+import { GetServerSideProps } from 'next';
+import ProjectSidebar from '@/components/project_sidebar/project_sidebar';
 import JournalDetail from '@/components/journal_detail/journal_detail';
 
-interface IJournalDetailPageProps {
+interface IProjectJournalPageProps {
+  projectId: string;
   journalId: string;
 }
 
-const JournalDetailPage = ({ journalId }: IJournalDetailPageProps) => {
+const ProjectJournalDetailPage = ({ projectId, journalId }: IProjectJournalPageProps) => {
   return (
     <>
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon/favicon.ico" />
-        {/* TODO: (20240503 - Julian) i18n */}
-        <title>Journal {journalId} - iSunFA</title>
+        {/* TODO: (2024606 - Julian) i18n */}
+        <title>Project Add Journal - iSunFA</title>
       </Head>
 
       <div className="h-screen font-barlow">
@@ -26,11 +27,11 @@ const JournalDetailPage = ({ journalId }: IJournalDetailPageProps) => {
         </div>
 
         <div className="flex w-full flex-1 flex-col overflow-x-hidden">
-          {/* Info: (20240503 - Julian) Sidebar */}
-          <AccountingSidebar />
-          {/* Info: (20240503 - Julian) Overview */}
-          <div className="flex h-full w-full bg-gray-100">
-            <div className="mt-100px flex-1 md:ml-80px">
+          {/* Info: (20240621 - Julian) Sidebar */}
+          <ProjectSidebar projectId={projectId} />
+          {/* Info: (20240621- Julian) Main */}
+          <div className="flex min-h-screen bg-gray-100">
+            <div className="my-120px flex-1 md:ml-80px">
               <JournalDetail journalId={journalId} />
             </div>
           </div>
@@ -41,6 +42,11 @@ const JournalDetailPage = ({ journalId }: IJournalDetailPageProps) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ params, locale }) => {
+  if (!params || !params.projectId || typeof params.projectId !== 'string') {
+    return {
+      notFound: true,
+    };
+  }
   if (!params || !params.journalId || typeof params.journalId !== 'string') {
     return {
       notFound: true,
@@ -49,10 +55,11 @@ export const getServerSideProps: GetServerSideProps = async ({ params, locale })
 
   return {
     props: {
+      projectId: params.projectId,
       journalId: params.journalId,
       ...(await serverSideTranslations(locale as string, ['common'])),
     },
   };
 };
 
-export default JournalDetailPage;
+export default ProjectJournalDetailPage;
