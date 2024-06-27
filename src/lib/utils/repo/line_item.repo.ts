3ab045b71e1fd888869b9fad69d0a12c16ba8baf
Tiny_Murky <murky0830@gmail.com>
2024-06-27 16:59:13 +0800

    lineItemRepo

--- src/lib/utils/repo/line_item.repo.ts
+++ src/lib/utils/repo/line_item.repo.ts
@@ -1,40 +1,63 @@
-// import prisma from '@/client';
-// import { AccountType } from '@/constants/account';
-// import { timestampInSeconds } from '../common';
+import prisma from '@/client';
+import { AccountType } from '@/constants/account';
+import { Prisma } from '@prisma/client';
+import { setTimestampToDayEnd, setTimestampToDayStart } from '../common';
 
-// async function getSumOfLineItemsGroupByAccountInPrisma(
-//   companyId: number,
-//   type: AccountType,
-//   startDate: number,
-//   endDate: number,
-// ) {
-//   const startDateInSecond = timestampInSeconds(startDate);
-//   const endDateInSecond = timestampInSeconds(endDate);
+export async function getSumOfLineItemsGroupByAccountInPrisma(
+  companyId: number,
+  type: AccountType,
+  startDate: number,
+  endDate: number,
+): Promise<{
+  accountId: number;
+  amount: number;
+}[]> {
+  const startDateInSecond = setTimestampToDayStart(startDate);
+  const endDateInSecond = setTimestampToDayEnd(endDate);
 
-//   return prisma.lineItem.groupBy({
-//     by: ['accountId'],
-//     where: {
-//       account: {
-//         type,
-//       },
-//       voucher: {
-//         journal: {
-//           companyId,
-//           AND: [
-//             {
-//               date: {
-//                 gte: startDateInSecond,
-//               },
-//             },
-//             {
-//               date: {
-//                 lte: endDateInSecond,
-//               },
-//             },
-//           ],
-//         }
-//       }
-//     }
+  const where: Prisma.LineItemWhereInput = {
+    account: {
+      type,
+    },
+    voucher: {
+      journal: {
+        companyId,
+      },
+      createdAt: {
+        gte: startDateInSecond,
+        lte: endDateInSecond,
+      },
+    }
+  };
 
-//   });
-// }
+  let lineItems: {
+      accountId: number;
+      amount: number;
+  }[];
+
+  try {
+    const lineItemsFromDB = await prisma.lineItem.groupBy({
+      by: ['accountId'],
+      where,
+      _sum: {
+        amount: true,
+      },
+    });
+
+    lineItems = lineItemsFromDB.map((lineItem) => {
+      return {
+        accountId: lineItem.accountId,
+        // Info: (20240627 - Murky) _sum is supposed to have underscore-dangle
+        // eslint-disable-next-line no-underscore-dangle
+        amount: lineItem._sum.amount || 0,
+      };
+    });
+  } catch (error) {
+    // Depreciated: (20240627 - Murky) Debugging purpose
+    // eslint-disable-next-line no-console
+    console.log(error);
+    lineItems = [];
+  }
+
+  return lineItems;
+}
