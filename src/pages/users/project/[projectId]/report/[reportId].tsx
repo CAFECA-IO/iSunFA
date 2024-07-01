34ed8963b1a305758398d56a5bd386dde70abe0e
julian <julian890701@gmail.com>
2024-07-01 16:57:24 +0800

    Feat: enhance report list & item

---
+++ src/pages/users/project/[projectId]/report/[reportId].tsx
@@ -0,0 +1,60 @@
+import Head from 'next/head';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { GetServerSideProps } from 'next';
+import NavBar from '@/components/nav_bar/nav_bar';
+import ProjectSidebar from '@/components/project_sidebar/project_sidebar';
+
+interface IProjectReportDetailPageProps {
+  projectId: string;
+  reportId: string;
+}
+
+const ProjectReportDetailPage = ({ projectId, reportId }: IProjectReportDetailPageProps) => {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon/favicon.ico" />
+        {/* TODO: (2024606 - Julian) i18n */}
+        <title>Project Report {reportId} - iSunFA</title>
+      </Head>
+
+      <div className="h-screen font-barlow">
+        <div className="">
+          <NavBar />
+        </div>
+
+        <div className="flex w-full flex-1 flex-col overflow-x-hidden">
+          {/* Info: (20240701 - Julian) Sidebar */}
+          <ProjectSidebar projectId={projectId} />
+          {/* Info: (20240701- Julian) Main */}
+          <div className="flex min-h-screen bg-gray-100"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ params, locale }) => {
+  if (!params || !params.projectId || typeof params.projectId !== 'string') {
+    return {
+      notFound: true,
+    };
+  }
+  if (!params || !params.reportId || typeof params.reportId !== 'string') {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      projectId: params.projectId,
+      reportId: params.reportId,
+      ...(await serverSideTranslations(locale as string, ['common'])),
+    },
+  };
+};
+
+export default ProjectReportDetailPage;
