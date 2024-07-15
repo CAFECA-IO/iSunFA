d0ba8bdff1d052e31bd1d40c8b36a69bc4690d46
julian <julian890701@gmail.com>
2024-07-15 16:42:32 +0800

    Fix: fix accounting stepper ui

--- src/components/accounting_stepper/accounting_stepper.tsx
+++ src/components/accounting_stepper/accounting_stepper.tsx
@@ -24,27 +24,27 @@ const AccountingStepper = ({ step }: IAccountingStepperProps) => {
   return (
     <div className="relative flex items-center gap-40px md:gap-120px">
       {/* Info: (20240422 - Julian) Step 1 */}
-      <div className={`z-10 flex flex-col items-center gap-2px text-sm ${stepOneStyle}`}>
+      <div className={`z-10 flex w-80px flex-col items-center gap-2px text-sm ${stepOneStyle}`}>
         <Image src={stepOneSrc} width={30} height={30} alt="step_1_upload" />
         <p>{t('JOURNAL.UPLOAD')}</p>
       </div>
 
       {/* Info: (20240422 - Julian) Connecting Line */}
       <div
-        className={`absolute left-30px top-12px h-4px w-90px md:w-180px ${connectingLineStyle}`}
+        className={`absolute left-30px top-12px h-4px w-90px md:w-200px ${connectingLineStyle}`}
       ></div>
 
       {/* Info: (20240422 - Julian) Step 2 */}
-      <div className={`z-10 flex flex-col items-center gap-2px text-sm ${stepTwoStyle}`}>
+      <div className={`z-10 flex w-80px flex-col items-center gap-2px text-sm ${stepTwoStyle}`}>
         <Image src={stepTwoSrc} width={30} height={30} alt="step_2_fill_up_form" />
         <p>{t('JOURNAL.FILL_UP_FORM')}</p>
       </div>
 
       {/* Info: (20240422 - Julian) Connecting Line */}
-      <div className="absolute right-30px top-12px h-4px w-90px bg-lightGray6 md:w-180px"></div>
+      <div className="absolute right-30px top-12px h-4px w-90px bg-lightGray6 md:w-200px"></div>
 
       {/* Info: (20240422 - Julian) Step 3 */}
-      <div className="z-10 flex flex-col items-center gap-2px text-sm text-lightGray4">
+      <div className="z-10 flex w-80px flex-col items-center gap-2px text-sm text-lightGray4">
         <Image src="/icons/confirm.svg" width={30} height={30} alt="step_3_confirm" />
         <p>{t('JOURNAL.CONFIRM')}</p>
       </div>
