0f4c0a13b50abdf119a91f5523c3c5616736782a
gibbs.shih <gibbs.shih@isuncloud.com>
2024-07-16 18:34:02 +0800

    update get voucher folder content

--- src/interfaces/folder.ts
+++ src/interfaces/folder.ts
@@ -1,5 +1,13 @@
+import { IJournalListItem } from '@/interfaces/journal';
+import { ISalaryRecord } from '@/interfaces/salary_record';
+
 export interface IFolder {
   id: number;
   name: string;
   createdAt: number;
+}
+
+export interface IFolderContent extends IFolder {
+  voucher: IJournalListItem;
+  salaryRecordList: ISalaryRecord[];
 }
--- src/lib/utils/repo/folder.repo.ts
+++ src/lib/utils/repo/folder.repo.ts
@@ -1,5 +1,5 @@
 import prisma from '@/client';
-import { IFolder } from '@/interfaces/folder';
+import { IFolder, IFolderContent } from '@/interfaces/folder';
 
 export async function getFolderList(companyId: number): Promise<IFolder[]> {
   const folderList = await prisma.voucherSalaryRecordFolder.findMany({
@@ -15,4 +15,160 @@ export async function getFolderList(companyId: number): Promise<IFolder[]> {
     };
   });
   return formattedFolderList;
+}
+
+export async function updateFolderName(
+  companyId: number,
+  folderId: number,
+  name: string
+): Promise<IFolder | null> {
+  const updatedFolder = await prisma.voucherSalaryRecordFolder.update({
+    where: {
+      id: folderId,
+      companyId,
+    },
+    data: {
+      name,
+    },
+  });
+  if (!updatedFolder) {
+    return null;
+  }
+  return {
+    id: updatedFolder.id,
+    name: updatedFolder.name,
+    createdAt: updatedFolder.createdAt,
+  };
+}
+
+export async function getFolderContent(
+  companyId: number,
+  folderId: number
+): Promise<IFolderContent | null> {
+  const salaryRecordList = await prisma.voucherSalaryRecord.findMany({
+    where: {
+      voucherSalaryRecordFolderId: folderId,
+      voucherSalaryRecordFolder: {
+        companyId,
+      },
+    },
+    select: {
+      voucherSalaryRecordFolder: {
+        select: {
+          id: true,
+          name: true,
+          createdAt: true,
+        },
+      },
+      salaryRecord: {
+        select: {
+          id: true,
+          employeeId: true,
+          employee: {
+            select: {
+              name: true,
+              department: {
+                select: {
+                  name: true,
+                },
+              },
+              company: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+          salary: true,
+          insurancePayment: true,
+          bonus: true,
+          description: true,
+          startDate: true,
+          endDate: true,
+          workingHour: true,
+          confirmed: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      },
+    },
+  });
+  const voucher = await prisma.voucherSalaryRecord.findFirst({
+    where: {
+      voucherSalaryRecordFolderId: folderId,
+    },
+    select: {
+      voucherId: true,
+      voucher: {
+        select: {
+          no: true,
+          createdAt: true,
+        },
+      },
+    },
+  });
+  if (!voucher) {
+    return null;
+  }
+  const voucherIdNumber = Number(voucher.voucherId);
+
+  const lineItems = await prisma.lineItem.findMany({
+    where: {
+      voucherId: voucherIdNumber,
+    },
+    select: {
+      debit: true,
+      amount: true,
+      account: {
+        select: {
+          code: true,
+          name: true,
+        },
+      },
+    },
+  });
+  const accounts = lineItems.map((lineItem) => {
+    return {
+      id: Number(lineItem.account.code),
+      debit: lineItem.debit,
+      account: lineItem.account.name,
+      amount: lineItem.amount,
+    };
+  });
+  const folderContent = {
+    id: folderId,
+    name: salaryRecordList[0].voucherSalaryRecordFolder.name,
+    createdAt: salaryRecordList[0].voucherSalaryRecordFolder.createdAt,
+    voucher: {
+      id: voucherIdNumber,
+      date: voucher.voucher.createdAt,
+      type: 'Payment',
+      particulars: 'Salary Bookkeeping',
+      fromTo: 'Employees',
+      account: accounts,
+      projectName: undefined,
+      projectImageId: undefined,
+      voucherId: voucherIdNumber,
+      voucherNo: voucher.voucher.no,
+    },
+    salaryRecordList: salaryRecordList.map((record) => {
+      return {
+        id: record.salaryRecord.id,
+        employeeId: record.salaryRecord.employeeId,
+        employeeName: record.salaryRecord.employee.name,
+        employeeDepartment: record.salaryRecord.employee.department.name,
+        salary: record.salaryRecord.salary,
+        insurancePayment: record.salaryRecord.insurancePayment,
+        bonus: record.salaryRecord.bonus,
+        description: record.salaryRecord.description,
+        startDate: record.salaryRecord.startDate,
+        endDate: record.salaryRecord.endDate,
+        workingHour: record.salaryRecord.workingHour,
+        confirmed: record.salaryRecord.confirmed,
+        createdAt: record.salaryRecord.createdAt,
+        updatedAt: record.salaryRecord.updatedAt,
+      };
+    }),
+  };
+  return folderContent;
 }
