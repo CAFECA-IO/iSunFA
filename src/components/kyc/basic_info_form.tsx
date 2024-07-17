0f02c1f741d4ecd106f53d484cc8a03b7fb4ff31
Liz <mingreal75@gmail.com>
2024-07-17 10:46:35 +0800

    adjust dropdown menu of city for BasicInfoForm

--- src/components/kyc/basic_info_form.tsx
+++ src/components/kyc/basic_info_form.tsx
@@ -1,22 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { CityType } from '@/constants/kyc';
+import { CityOptions } from '@/constants/kyc';
 import { BasicInfoKeys, IBasicInfo } from '@/interfaces/kyc_basic_info';
 import useOuterClick from '@/lib/hooks/use_outer_click';
 import { useState } from 'react';
 import { FaChevronDown } from 'react-icons/fa';
 import { useTranslation } from 'next-i18next';
 
-// cities
-const cites = [
-  { code: 'GB', name: 'United Kingdom', flag: '🇬🇧' },
-  { code: 'US', name: 'United States', flag: '🇺🇸' },
-  { code: 'TW', name: 'Taiwan', flag: '🇹🇼' },
-];
-
-const cityTypeMap: { [key in CityType]: string } = {
-  [CityType.GB]: 'KYC.GB',
-  [CityType.US]: 'KYC.US',
-  [CityType.TW]: 'KYC.TW',
+const cityTypeMap: { [key in CityOptions]: string } = {
+  [CityOptions.DEFAULT]: 'KYC.DEFAULT',
+  [CityOptions.GB]: 'KYC.GB',
+  [CityOptions.US]: 'KYC.US',
+  [CityOptions.TW]: 'KYC.TW',
 };
 
 const BasicInfoForm = ({
@@ -29,8 +23,6 @@ const BasicInfoForm = ({
   onChange: (key: BasicInfoKeys, value: string) => void;
 }) => {
   const { t } = useTranslation('common');
-  // 狀態管理
-  const [selectedCity, setSelectedCity] = useState<CityType>(CityType.TW);
 
   const {
     targetRef: cityMenuRef,
@@ -42,32 +34,32 @@ const BasicInfoForm = ({
   const cityMenuOpenHandler = () => setIsCityMenuOpen(!isCityMenuOpen);
 
   // 下拉選單選項
-  const cityDropmenu = Object.values(CityType).map((type: CityType) => {
+  const cityDropmenu = Object.values(CityOptions).map((city: CityOptions) => {
     const selectionClickHandler = () => {
-      setSelectedCity(type);
+      onChange(BasicInfoKeys.CITY, city);
       setIsCityMenuOpen(false);
     };
 
     return (
       <li
-        key={type}
+        key={city}
         onClick={selectionClickHandler}
-        className="w-full cursor-pointer px-3 py-2 text-navyBlue2 hover:text-primaryYellow"
+        className={`w-full cursor-pointer px-3 py-2 text-navyBlue2 hover:text-primaryYellow ${city === CityOptions.DEFAULT ? 'hidden' : ''}`}
       >
-        <p>{t(cityTypeMap[type])}</p>
+        <p>{t(cityTypeMap[city])}</p>
       </li>
     );
   });
 
   return (
-    <section className="flex flex-col border-4 border-lime-400">
+    <section className="flex w-600px flex-col border-4 border-lime-400">
       <div>
         <p>Legal Company Name</p>
         <input type="text" placeholder="example" required className="w-full" />
       </div>
 
       <div className="flex gap-20px">
-        {/* ===== Info: City Type ===== */}
+        {/* ===== Info: City ===== */}
         <div className="flex flex-1 flex-col items-start gap-8px">
           <p className="text-sm font-semibold text-navyBlue2">Company Address</p>
           <div
@@ -75,7 +67,11 @@ const BasicInfoForm = ({
             onClick={cityMenuOpenHandler}
             className={`group relative flex h-46px w-full cursor-pointer ${isCityMenuOpen ? 'border-primaryYellow text-primaryYellow' : 'border-lightGray3 text-navyBlue2'} items-center justify-between rounded-sm border bg-white p-10px hover:border-primaryYellow hover:text-primaryYellow`}
           >
-            <p>{t(cityTypeMap[selectedCity])}</p>
+            <p
+              className={`${data[BasicInfoKeys.CITY] === CityOptions.DEFAULT ? 'text-input-text-input-placeholder' : ''}`}
+            >
+              {t(cityTypeMap[data[BasicInfoKeys.CITY]])}
+            </p>
             <FaChevronDown />
             {/* Info: Dropmenu */}
             <div
--- src/constants/kyc.ts
+++ src/constants/kyc.ts
@@ -1,5 +1,43 @@
-export enum CityType {
+export enum CityOptions {
+  DEFAULT = '',
   GB = 'United Kingdom',
   US = 'United States',
   TW = 'Taiwan',
 }
+// Info: (240717 - Liz) 以下是暫存，等確定設計稿是國家還是城市後會再調整邏輯
+
+export enum CountryCode {
+  DEFAULT = '',
+  GB = 'GB',
+  US = 'US',
+  TW = 'TW',
+}
+
+interface ICountryInfo {
+  title: string;
+  svg: string;
+  translationKey: string;
+}
+
+export const CountryInfos: Record<CountryCode, ICountryInfo> = {
+  [CountryCode.DEFAULT]: {
+    title: '',
+    svg: '',
+    translationKey: 'KYC.DEFAULT',
+  },
+  [CountryCode.GB]: {
+    title: 'United Kingdom',
+    svg: '🇬🇧',
+    translationKey: 'KYC.GB',
+  },
+  [CountryCode.US]: {
+    title: 'United States',
+    svg: '🇺🇸',
+    translationKey: 'KYC.US',
+  },
+  [CountryCode.TW]: {
+    title: 'Taiwan',
+    svg: '🇹🇼',
+    translationKey: 'KYC.TW',
+  },
+} as const;
