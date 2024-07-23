4ada74d051dcbb06ef2b067d011c53a3c23fbc3e
Luphia Chang <luphia@cafeca.io>
2024-07-23 21:16:53 +0800

    for test compile

--- src/pages/api/v1/company/[companyId]/file/index.ts
+++ src/pages/api/v1/company/[companyId]/file/index.ts
@@ -1,11 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { FileFolder } from '@/constants/file';
 import { STATUS_MESSAGE } from '@/constants/status_code';
 import { IResponseData } from '@/interfaces/response_data';
 import { IFile } from '@/interfaces/file';
-import { getSession } from '@/lib/utils/session';
-import { checkUserAdmin } from '@/lib/utils/auth_check';
-import { addPrefixToFile, parseForm } from '@/lib/utils/parse_image_form';
 import { formatApiResponse } from '@/lib/utils/common';
 
 export const config = {
@@ -15,38 +11,13 @@ export const config = {
 };
 
 async function handlePostRequest(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   req: NextApiRequest,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   res: NextApiResponse<IResponseData<IFile | null>>
 ): Promise<{ statusMessage: string; payload: IFile | null }> {
-  let statusMessage: string = STATUS_MESSAGE.BAD_REQUEST;
-  let payload: IFile | null = null;
-
-  const session = await getSession(req, res);
-  const { userId, companyId } = session;
-  const companyIdStr = companyId.toString();
-  const isAuth = await checkUserAdmin({ userId, companyId });
-  if (!isAuth) {
-    statusMessage = STATUS_MESSAGE.UNAUTHORIZED_ACCESS;
-  } else {
-    try {
-      const parsedForm = await parseForm(req, FileFolder.TMP);
-      const { files } = parsedForm;
-      const { file } = files;
-
-      if (!file) {
-        statusMessage = STATUS_MESSAGE.IMAGE_UPLOAD_FAILED_ERROR;
-      } else {
-        // Info (20240722 - Jacky) add prefix to file name for get by company id
-        const ext = file && file[0] && file[0].mimetype ? file[0].mimetype.split('/')[1] : '';
-        await addPrefixToFile(FileFolder.TMP, file[0].newFilename, companyIdStr, ext);
-        payload = { id: file[0].newFilename, size: file[0].size, existed: true };
-        statusMessage = STATUS_MESSAGE.CREATED;
-      }
-    } catch (error) {
-      // 错误处理保持不变
-      statusMessage = STATUS_MESSAGE.INTERNAL_SERVICE_ERROR;
-    }
-  }
+  const statusMessage: string = STATUS_MESSAGE.BAD_REQUEST;
+  const payload: IFile | null = null;
 
   return { statusMessage, payload };
 }
