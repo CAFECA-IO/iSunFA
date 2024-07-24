8edd372bbc2c82b50d5d2bbff50911c76ef61b4a
Luphia Chang <luphia@mermer.cc>
2024-07-24 13:35:26 +0800

    Update journal_list_body.tsx

--- src/components/journal_list_body/journal_list_body.tsx
+++ src/components/journal_list_body/journal_list_body.tsx
@@ -127,7 +127,9 @@ const JournalListBody = () => {
           endDate: !(period ?? filteredPeriod).endTimeStamp
             ? undefined
             : (period ?? filteredPeriod).endTimeStamp,
-          searchQuery: !(searchString ?? search) ? undefined : searchString ?? search,
+          searchQuery: !(searchString ?? search)
+            ? undefined
+            : (searchString ?? search),
         },
       });
       setSuccess(response.success);
