b62729df64947f1b75d3c05d89654435b95e3a2e
jing12345678910 <onetwo12826@gmail.com>
2024-07-30 22:49:56 +0800

    Modify the translation of report pages

--- src/components/view_financial_section/view_financial_section.tsx
+++ src/components/view_financial_section/view_financial_section.tsx
@@ -253,6 +253,13 @@ const ViewFinancialSection = ({
     <>{ReportSheetTypeDisplayMap[reportFinancial?.reportType ?? ReportSheetType.BALANCE_SHEET]}</>
   );
 
+  // Info:創建一個新的變數來儲存翻譯後的字串 (20240730 - Anna)
+  const reportTypeString =
+    !getReportFinancialIsLoading && typeof displayedReportType.props.children === 'string'
+      ? displayedReportType.props.children
+      : '';
+  const translatedReportType = t(`PLUGIN.${reportTypeString.toUpperCase().replace(/ /g, '_')}`);
+
   const renderedThumbnail = (
     thumbnail: { number: number; active: boolean; alt: string; src: string },
     index: number
@@ -279,6 +286,10 @@ const ViewFinancialSection = ({
     </button>
   );
 
+  {
+    /* Info: 如果 displayedReportType 是字串，就使用它的值；如果不是字串，就設置為空字串。 (20240730 - Anna) */
+  }
+  // const reportTypeString = typeof displayedReportType === 'string' ? displayedReportType : '';
   const displayedReport = (
     <div className="mt-12 flex h-850px w-full bg-surface-neutral-main-background px-5 pb-2 md:px-0 lg:px-40">
       {/* Info: Sidebar (20240426 - Shirley) */}
@@ -335,8 +346,15 @@ const ViewFinancialSection = ({
             </svg>
           </div>
         </Button>
-        <div className="flex-1 justify-center self-stretch text-lg font-semibold leading-10 text-slate-500 max-md:max-w-full lg:text-4xl">
+        {/* <div className="flex-1 justify-center self-stretch text-lg font-semibold leading-10 text-slate-500 max-md:max-w-full lg:text-4xl">
           {displayedReportType}
+        </div> */}
+        <div className="flex-1 justify-center self-stretch text-lg font-semibold leading-10 text-slate-500 max-md:max-w-full lg:text-4xl">
+          {getReportFinancialIsLoading ? (
+            <Skeleton width={200} height={40} />
+          ) : (
+            translatedReportType
+          )}
         </div>
         <div className="my-auto flex flex-col justify-center self-stretch">
           <div className="flex gap-3">
