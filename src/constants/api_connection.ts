19a6e5a78d033f19c47f35ecd85a7e982a84d818
Liz <mingreal75@gmail.com>
2024-08-20 14:31:03 +0800

    update folder and file name (part 1)

--- src/constants/api_connection.ts
+++ src/constants/api_connection.ts
@@ -84,9 +84,9 @@ export enum APIName {
 export enum APIPath {
   AGREE_TO_TERMS = `${apiPrefix}/user/:userId/agreement`,
   CREATE_CHALLENGE = `${apiPrefix}/challenge`,
-  SIGN_UP = `${apiPrefix}/sign-up`,
-  SIGN_IN = `${apiPrefix}/sign-in`,
-  SIGN_OUT = `${apiPrefix}/sign-out`,
+  SIGN_UP = `${apiPrefix}/sign_up`,
+  SIGN_IN = `${apiPrefix}/sign_in`,
+  SIGN_OUT = `${apiPrefix}/sign_out`,
   EMAIL = `${apiPrefix}/email`,
   USER_GET_BY_ID = `${apiPrefix}/user/:userId`,
   USER_UPDATE = `${apiPrefix}/user/:userId`,
