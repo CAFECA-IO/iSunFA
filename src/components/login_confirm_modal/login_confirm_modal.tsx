8b36d771fd2721db99ce459a42e8e2b5574ace0a
TzuHanLiang <crocodilestear@gmail.com>
2024-08-20 09:58:39 +0800

    feat: connect agreement api

--- src/components/login_confirm_modal/login_confirm_modal.tsx
+++ src/components/login_confirm_modal/login_confirm_modal.tsx
@@ -1,29 +1,51 @@
 import React from 'react';
 import { RxCross2 } from 'react-icons/rx';
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'next-i18next';
 import InformationStatement from '@/components/login_confirm_modal/information_statement';
 import TermsOfServiceAndPrivacyPolicy from '@/components/login_confirm_modal/term_n_privacy';
+import { useUserCtx } from '@/contexts/user_context';
+import { Hash } from '@/constants/hash';
 
 interface ILoginConfirmProps {
+  id: string;
   isModalVisible: boolean;
   modalData: {
     title: string;
     content: string;
     buttonText: string;
   };
-  onAgree: () => void;
-  onCancel: () => void;
+  infoModalVisibilityHandler: (visibility: boolean) => void;
+  tosModalVisibilityHandler: (visibility: boolean) => void;
 }
 
 const LoginConfirmModal: React.FC<ILoginConfirmProps> = ({
+  id,
   isModalVisible,
   modalData,
-  onAgree,
-  onCancel,
+  infoModalVisibilityHandler,
+  tosModalVisibilityHandler,
 }) => {
   const { t } = useTranslation('common');
+  const { handleUserAgree, signOut } = useUserCtx();
+
+  const onAgree = async () => {
+    if (id === 'agree-with-information') {
+      infoModalVisibilityHandler(false);
+      await handleUserAgree(Hash.INFO_COLLECTION);
+      tosModalVisibilityHandler(true);
+    }
+    if (id === 'tos-n-privacy-policy') {
+      tosModalVisibilityHandler(false);
+      await handleUserAgree(Hash.TOS_N_PP);
+    }
+  };
+  const onCancel = () => {
+    infoModalVisibilityHandler(false);
+    tosModalVisibilityHandler(false);
+    signOut();
+  };
   const displayModal = isModalVisible ? (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+    <div id={id} className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
       <div className="absolute max-h-80vh w-full max-w-xl rounded-xs bg-white p-4 pt-0 shadow-lg">
         <div className="my-4 mb-8 flex items-center justify-between">
           <h2 className="text-lg font-semibold text-navyBlue">{modalData.title}</h2>
@@ -32,7 +54,7 @@ const LoginConfirmModal: React.FC<ILoginConfirmProps> = ({
           </button>
         </div>
         <hr className="absolute left-0 top-60px w-full max-w-xl border-lightGray6" />
-        <div className="h-50vh m-4 overflow-y-auto rounded-xs border border-navyBlue bg-lightGray7 lg:p-4">
+        <div className="m-4 h-50vh overflow-y-auto rounded-xs border border-navyBlue bg-lightGray7 lg:p-4">
           {modalData.content === 'info_collection_statement' && <InformationStatement />}
           {modalData.content === 'term_n_privacy' && <TermsOfServiceAndPrivacyPolicy />}
         </div>
