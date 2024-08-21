9abd9a1938c3999024331b5094e42f9fb43ac80d
Luphia Chang <luphia@mermer.cc>
2024-08-21 21:35:20 +0800

    Update journal_upload_area.tsx

--- src/components/journal_upload_area/journal_upload_area.tsx
+++ src/components/journal_upload_area/journal_upload_area.tsx
@@ -89,7 +89,6 @@ const JournalUploadArea = () => {
       formData.append('imageSize', uploadFile.size);
       formData.append('imageName', uploadFile.name);
       formData.append('uploadIdentifier', uuid);
-      // console.log('formData', formData);
 
       // Info: (20240711 - Julian) 點擊上傳後才升起 flag
       // setIsShowSuccessModal(true);
