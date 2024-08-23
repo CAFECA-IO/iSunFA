5227d11d5715f24ad05a47808b97807996199e9d
arealclimber <luffulgence@gmail.com>
2024-08-23 17:32:19 +0800

    feat: upload the pending uploaded file stored on IndexedDB #2187 #2077

--- src/components/journal_upload_area/journal_upload_area.tsx
+++ src/components/journal_upload_area/journal_upload_area.tsx
@@ -12,7 +12,7 @@ import { MessageType } from '@/interfaces/message_modal';
 import { ToastType } from '@/interfaces/toastify';
 import { getTimestampNow, transformBytesToFileSizeString } from '@/lib/utils/common';
 import { encryptFile, exportPublicKey, generateKeyPair, importPublicKey } from '@/lib/utils/crypto';
-import { addItem, deleteItem } from '@/lib/utils/indexed_db/ocr';
+import { addItem, deleteItem, getAllItems } from '@/lib/utils/indexed_db/ocr';
 import { IOCR, IOCRItem } from '@/interfaces/ocr';
 
 interface FileInfo {
@@ -41,6 +41,23 @@ const JournalUploadArea = () => {
   // const [isShowSuccessModal, setIsShowSuccessModal] = useState<boolean>(false);
   // Info: (20240711 - Julian) 拖曳的樣式
   const [isDragOver, setIsDragOver] = useState<boolean>(false);
+  const [dataStoredToIndexedDB, setDataStoredToIndexedDB] = useState<IOCRItem | null>(null);
+
+  const generateKeyPairAndEncryptFile = async (fileArrayBuffer: ArrayBuffer) => {
+    // TODO: 暫時生成 RSA 密鑰對，但 public key 應該要從後端來 (20240822 - Shirley)
+    const keyPair = await generateKeyPair();
+    const publicKey = await exportPublicKey(keyPair.publicKey);
+    // Info: 生成初始向量 (20240822 - Shirley)
+    const iv = crypto.getRandomValues(new Uint8Array(12));
+
+    const { encryptedContent, encryptedSymmetricKey } = await encryptFile(
+      fileArrayBuffer,
+      await importPublicKey(publicKey),
+      iv
+    );
+
+    return { encryptedContent, encryptedSymmetricKey, publicKey, iv };
+  };
 
   const encryptFileAndSaveToIndexedDB = async (file: File) => {
     // Info: 檢查是否有使用者和公司資訊 (20240822 - Shirley)
@@ -53,24 +70,13 @@ const JournalUploadArea = () => {
     // Info: 讀取文件內容為 ArrayBuffer (20240822 - Shirley)
     const fileArrayBuffer = await file.arrayBuffer();
     const uuid = uuidv4();
-    // Info: 生成 RSA 密鑰對 (20240822 - Shirley)
-    const keyPair = await generateKeyPair();
-    const publicKey = await exportPublicKey(keyPair.publicKey);
-
-    // Info: 生成初始向量 (20240822 - Shirley)
-    const iv = crypto.getRandomValues(new Uint8Array(12));
-
-    // Info: 使用混合加密方法加密文件 (20240822 - Shirley)
-    const { encryptedContent, encryptedSymmetricKey } = await encryptFile(
-      fileArrayBuffer,
-      await importPublicKey(publicKey),
-      iv
-    );
+    const { encryptedContent, encryptedSymmetricKey, publicKey, iv } =
+      await generateKeyPairAndEncryptFile(fileArrayBuffer);
 
     // Info: 將加密後的文件數據存儲到 IndexedDB (20240822 - Shirley)
     const now = getTimestampNow();
-    const testId = uuid;
-    const testData: IOCRItem = {
+    const id = uuid;
+    const data: IOCRItem = {
       name: file.name,
       size: fileSize,
       type: file.type,
@@ -83,7 +89,9 @@ const JournalUploadArea = () => {
       iv,
       userId: userAuth?.id || -1,
     };
-    await addItem(testId, testData);
+    setDataStoredToIndexedDB(data);
+    // setDataStoredToIndexedDB([...dataStoredToIndexedDB, data]);
+    await addItem(id, data);
   };
 
   // Info: (20240711 - Julian) 處理上傳檔案
@@ -98,8 +106,6 @@ const JournalUploadArea = () => {
         name: file.name,
         size: fileSize,
       });
-
-      encryptFileAndSaveToIndexedDB(file);
     }
   };
   // Info: (20240711 - Julian) 處理拖曳上傳檔案
@@ -128,6 +134,17 @@ const JournalUploadArea = () => {
     }
   };
 
+  const handleAllDataFromIndexedDB = async () => {
+    const data = await getAllItems();
+    // eslint-disable-next-line no-console
+    console.log('data in handleAllDataFromIndexedDB', data);
+    // setDataStoredToIndexedDB(data.map((item) => item.data));
+  };
+
+  useEffect(() => {
+    handleAllDataFromIndexedDB();
+  }, []);
+
   useEffect(() => {
     if (uploadFile && selectedCompany) {
       const formData = new FormData();
@@ -136,11 +153,18 @@ const JournalUploadArea = () => {
       formData.append('imageSize', uploadFile.size);
       formData.append('imageName', uploadFile.name);
       formData.append('uploadIdentifier', uuid);
+      if (!dataStoredToIndexedDB) return;
+      // eslint-disable-next-line no-console
+      console.log('dataStoredToIndexedDB', dataStoredToIndexedDB);
+
+      formData.append('encryptedSymmetricKey', dataStoredToIndexedDB.encryptedSymmetricKey);
+      formData.append('publicKey', JSON.stringify(dataStoredToIndexedDB.publicKey));
+      formData.append('iv', Array.from(dataStoredToIndexedDB.iv).join(','));
 
       addPendingOCRHandler(uploadFile.name, uploadFile.size, uuid);
       uploadInvoice({ params: { companyId: selectedCompany.id }, body: formData });
     }
-  }, [uploadFile]);
+  }, [uploadFile, dataStoredToIndexedDB]);
 
   useEffect(() => {
     if (uploadSuccess && results) {
