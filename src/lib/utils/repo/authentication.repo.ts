5ba7a213a940d9c040a8aa44912f2bffe4bd9269
Luphia Chang <luphia@mermer.cc>
2024-08-23 22:05:21 +0800

    Update authentication.repo.ts

--- src/lib/utils/repo/authentication.repo.ts
+++ src/lib/utils/repo/authentication.repo.ts
@@ -47,8 +47,6 @@ export async function createUserByAuth({
 }): Promise<Authentication & { user: User & { userAgreements: UserAgreement[] } }> {
   const now = Date.now();
   const nowTimestamp = timestampInSeconds(now);
-  // publicKey: string, // Info: (20240816 - Jacky)
-  // algorithm: string, // Info: (20240816 - Jacky)
 
   const createdAuthentication = await prisma.authentication.create({
     data: {
