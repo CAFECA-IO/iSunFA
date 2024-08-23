4e999614d9197e3ec0d9b545f4358cf2bc661eec
Luphia Chang <luphia@mermer.cc>
2024-08-23 22:14:51 +0800

    Update cash_flow_statement_generator.ts

--- src/lib/utils/report/cash_flow_statement_generator.ts
+++ src/lib/utils/report/cash_flow_statement_generator.ts
@@ -23,15 +23,10 @@ import { SPECIAL_ACCOUNTS } from '@/constants/account';
 
 export default class CashFlowStatementGenerator extends FinancialReportGenerator {
   private balanceSheetGenerator: BalanceSheetGenerator;
-
   private incomeStatementGenerator: IncomeStatementGenerator;
-
   private voucherRelatedToCash: IVoucherFromPrismaIncludeJournalLineItems[];
-
   private voucherLastPeriod: IVoucherFromPrismaIncludeJournalLineItems[];
-
   private YEAR_RANGE = 5;
-
   private eslintEscape = '';
 
   constructor(
@@ -640,7 +635,6 @@ export default class CashFlowStatementGenerator extends FinancialReportGenerator
       EMPTY_I_ACCOUNT_READY_FRONTEND;
     const saleFVOCI =
       accountMap.get(SPECIAL_ACCOUNTS.CASH_DISPOSE_FVOCI.code) || EMPTY_I_ACCOUNT_READY_FRONTEND;
-    // const saleFVPL = accountMap.get('B00200') || EMPTY_I_ACCOUNT_READY_FRONTEND; // Info: (20240729 - Murky)
     const saleAmortizedFA =
       accountMap.get(SPECIAL_ACCOUNTS.CASH_DISPOSE_AMORTIZED_FINANCIAL_ASSET.code) ||
       EMPTY_I_ACCOUNT_READY_FRONTEND;
@@ -650,7 +644,6 @@ export default class CashFlowStatementGenerator extends FinancialReportGenerator
     const receiveStockDividend =
       accountMap.get(SPECIAL_ACCOUNTS.CASH_RECEIVE_STOCK_DIVIDEND.code) ||
       EMPTY_I_ACCOUNT_READY_FRONTEND;
-    // const equityDividend = accountMap.get('xxxx') || EMPTY_I_ACCOUNT_READY_FRONTEND; <= //  Info: (20240729 - Murky) 沒有這個項目
     const totalInvestCashFlow =
       accountMap.get(SPECIAL_ACCOUNTS.CASH_FLOW_FROM_INVESTING.code) ||
       EMPTY_I_ACCOUNT_READY_FRONTEND;
