e14b900ac8446a0cd2b1386bab95a9b97ba1f685
gibbs.shih <gibbs.shih@isuncloud.com>
2024-08-23 11:57:15 +0800

    update file.ts

--- src/lib/utils/file.ts
+++ src/lib/utils/file.ts
@@ -1,30 +1,14 @@
-<<<<<<< HEAD
 import { LOG_FOLDER, UPLOAD_IMAGE_FOLDERS_TO_CREATE_WHEN_START_SERVER } from '@/constants/file';
-=======
-import { UPLOAD_IMAGE_FOLDERS_TO_CREATE_WHEN_START_SERVER } from '@/constants/file';
->>>>>>> develop
 import { promises as fs } from 'fs';
 
 export async function createFileFoldersIfNotExists(): Promise<void> {
   UPLOAD_IMAGE_FOLDERS_TO_CREATE_WHEN_START_SERVER.map(async (folder) => {
     try {
       await fs.mkdir(folder, { recursive: true });
-<<<<<<< HEAD
-      // Deprecate: (20240812 - Murky): Debugging purpose
-      // eslint-disable-next-line no-console
-      console.log(`Folder ${folder} created.`);
+      // Todo: (20240822 - Anna) feat. Murky - 使用 logger
     } catch (error) {
-      // Deprecate: (20240812 - Murky): Debugging purpose
-      // eslint-disable-next-line no-console
-      console.error(`Error while creating folder: ${error}`);
+      // Todo: (20240822 - Anna) feat. Murky - 使用 logger
     }
   });
   await fs.mkdir(LOG_FOLDER, { recursive: true });
-=======
-      // Todo: (20240822 - Anna) feat. Murky - 使用 logger
-    } catch (error) {
-      // Todo: (20240822 - Anna) feat. Murky - 使用 logger
-    }
-  });
->>>>>>> develop
 }
