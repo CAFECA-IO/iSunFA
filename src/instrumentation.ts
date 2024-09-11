fca970c69eec0885aef16a10fc09866b8db8aa79
gibbs.shih <gibbs.shih@isuncloud.com>
2024-09-11 10:39:40 +0800

    update instrumentation

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,10 +1,7 @@
-import loggerBack from '@/lib/utils/logger_back';
-
 /**
  * Info: (20240812 - Murky) All code in register will be run when "Starting..." is printed in the console.
  */
 export async function register() {
-  loggerBack.info('Instrumentation register starting...');
   // Info: (20240812 - Murky) Node module using in this file need to be dynamically imported, it must be inside "if" statement
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { createFileFoldersIfNotExists } = await import('@/lib/utils/file');
@@ -12,5 +9,4 @@ export async function register() {
     await import('pino');
     await import('next-logger');
   }
-  loggerBack.info('Instrumentation register completed!');
 }
