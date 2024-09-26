3ebb2085f4e8cfe826ccd622447d2b36e243b309
Liz <mingreal75@gmail.com>
2024-09-26 13:01:41 +0800

    beta login UI : LoginPage & LoginPageBody

---
+++ src/components/beta/login/login_page_body.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import Image from 'next/image';
+import AvatarSVG from '@/components/avatar_svg/avatar_svg';
+import { useTranslation } from 'next-i18next';
+import { FiHome } from 'react-icons/fi';
+import I18n from '@/components/i18n/i18n';
+
+const isAuthLoading = false;
+
+const Loader = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="h-16 w-16 animate-spin rounded-full border-4 border-orange-400 border-t-transparent"></div>
+    </div>
+  );
+};
+
+const LoginPageBody = () => {
+  const { t } = useTranslation('common');
+
+  return (
+    <div className="relative flex h-screen flex-col items-center justify-center text-center">
+      <div className="absolute inset-0 z-0 h-full w-full bg-login_bg bg-cover bg-center bg-no-repeat blur-md"></div>
+
+      <div className="absolute right-0 top-0 z-0 mr-40px mt-40px flex items-center gap-40px text-button-text-secondary">
+        <I18n />
+
+        <FiHome size={20} />
+      </div>
+
+      {isAuthLoading ? (
+        <Loader />
+      ) : (
+        <div className="z-10 flex flex-col items-center">
+          <div className="mb-80px flex gap-10px text-48px font-bold">
+            <p className="text-text-brand-primary-lv2">iSunFA</p>
+            <p className="text-text-brand-secondary-lv1">{t('common:LOGIN_PAGE_BODY.LOG_IN')}</p>
+          </div>
+
+          <div className="mb-40px flex flex-col justify-center rounded-full">
+            <AvatarSVG size="large" />
+          </div>
+
+          <div className="flex w-360px flex-col gap-16px">
+            <button
+              type="button"
+              className="flex items-center justify-center gap-15px rounded-sm bg-white p-15px"
+            >
+              <Image src="/icons/google_logo.svg" alt="google_logo" width="24" height="24"></Image>
+              <p className="text-xl font-medium text-gray-500">Log In with Google</p>
+            </button>
+
+            <button
+              type="button"
+              className="flex items-center justify-center gap-15px rounded-sm bg-black p-15px"
+            >
+              <Image src="/icons/apple_logo.svg" alt="apple_logo" width="24" height="24"></Image>
+              <p className="text-xl font-medium text-white">Log In with Apple</p>
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LoginPageBody;
