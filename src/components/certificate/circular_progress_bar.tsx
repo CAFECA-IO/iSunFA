afaa3cc8ee3fe73d2ae4f2492d89e7b60b687f22
Luphia Chang <luphia@mermer.cc>
2024-09-26 19:23:17 +0800

    Update circular_progress_bar.tsx

--- src/components/certificate/circular_progress_bar.tsx
+++ src/components/certificate/circular_progress_bar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 interface CircularProgressBarProps {
-  size: number; // 圓形直徑
-  progress: number; // 進度百分比
-  strokeWidth: number; // 線條寬度
-  remainingText: string; // 顯示的剩餘文字
+  size: number; // Info: (20240926 - tzuhan) 圓形直徑
+  progress: number; // Info: (20240926 - tzuhan) 進度百分比
+  strokeWidth: number; // Info: (20240926 - tzuhan) 線條寬度
+  remainingText: string; // Info: (20240926 - tzuhan) 顯示的剩餘文字
 }
 
 const CircularProgressBar: React.FC<CircularProgressBarProps> = ({
@@ -13,11 +13,11 @@ const CircularProgressBar: React.FC<CircularProgressBarProps> = ({
   strokeWidth,
   remainingText,
 }) => {
-  // 圓的周長
+  // Info: (20240926 - tzuhan) 圓的周長
   const radius = (size - strokeWidth) / 2;
   const circumference = radius * 2 * Math.PI;
 
-  // 根據進度計算 dashoffset
+  // Info: (20240926 - tzuhan) 根據進度計算 dashoffset
   const dashOffset = circumference - (progress / 100) * circumference;
 
   return (
@@ -27,7 +27,7 @@ const CircularProgressBar: React.FC<CircularProgressBarProps> = ({
           cx={size / 2}
           cy={size / 2}
           r={radius}
-          stroke="#D9D9D9" // 背景圓的顏色
+          stroke="#D9D9D9" // Info: (20240926 - tzuhan) 背景圓的顏色
           strokeWidth={strokeWidth}
           fill="transparent"
         />
@@ -35,12 +35,12 @@ const CircularProgressBar: React.FC<CircularProgressBarProps> = ({
           cx={size / 2}
           cy={size / 2}
           r={radius}
-          stroke="#FFA502" // 進度條的顏色
+          stroke="#FFA502" // Info: (20240926 - tzuhan) 進度條的顏色
           strokeWidth={strokeWidth}
           fill="transparent"
           strokeDasharray={circumference}
           strokeDashoffset={dashOffset}
-          strokeLinecap="round" // 圓形端點
+          strokeLinecap="round" // Info: (20240926 - tzuhan) 圓形端點
         />
       </svg>
       <span className="ml-2 font-semibold text-gray-800">{remainingText}</span>
