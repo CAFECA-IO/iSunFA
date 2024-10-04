0cc18560cf0a188472d60ed8e9d47ae8847a43d8
arealclimber <luffulgence@gmail.com>
2024-10-04 16:00:10 +0800

    refactor: 💡 ledger, trial balance APIs

--- src/pages/api/v2/company/[companyId]/trial_balance/index.test.ts
+++ src/pages/api/v2/company/[companyId]/trial_balance/index.test.ts
@@ -1,8 +1,6 @@
 import { STATUS_MESSAGE } from '@/constants/status_code';
-import {
-  handleGetRequest,
-  MOCK_RESPONSE,
-} from '@/pages/api/v2/company/[companyId]/trial_balance/index';
+import { MOCK_RESPONSE } from '@/interfaces/trial_balance';
+import { handleGetRequest } from '@/pages/api/v2/company/[companyId]/trial_balance/index';
 
 describe('handleGetRequest', () => {
   it('Should return the correct trial balance data', async () => {
