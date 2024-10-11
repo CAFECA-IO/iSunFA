315411aece063b752ae58c0e8630759f0aa6ef6f
Liz <mingreal75@gmail.com>
2024-10-11 16:47:19 +0800

    update Layout

--- src/components/beta/layout/layout.tsx
+++ src/components/beta/layout/layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { FiLayout } from 'react-icons/fi';
 import Header from '@/components/beta/layout/header';
 
@@ -7,13 +7,34 @@ interface LayoutProps {
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(true);
+
+  const ToggleSideMenu = () => {
+    setIsSideMenuOpen((prev) => !prev);
+  };
+
   return (
     <div className="flex h-screen border-2 border-rose-400">
       {/* side menu */}
-      <section className="w-280px flex-none border-2 border-sky-400">
-        <FiLayout size={24} />
-        <div>Parameter Setting</div>
-      </section>
+
+      {isSideMenuOpen ? (
+        <section className="w-280px flex-none border-2 border-sky-400 px-12px py-32px">
+          <button type="button" className="p-10px">
+            <FiLayout size={24} onClick={ToggleSideMenu} />
+          </button>
+          <div>Parameter Setting</div>
+          <div>Parameter Setting</div>
+          <div>Parameter Setting</div>
+          <div>Parameter Setting</div>
+          <div>Parameter Setting</div>
+        </section>
+      ) : (
+        <section className="w-66px flex-none px-12px py-32px">
+          <button type="button" className="p-10px">
+            <FiLayout size={24} onClick={ToggleSideMenu} />
+          </button>
+        </section>
+      )}
 
       <div className="flex flex-auto flex-col gap-40px border-2 border-lime-400 bg-surface-neutral-main-background px-56px py-32px">
         <Header />
