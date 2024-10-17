4d4cbe8380287fd470197b0364ccf7478d8398d3
Liz <mingreal75@gmail.com>
2024-10-17 10:49:58 +0800

    quick fix layout for sidebar height

--- src/components/beta/layout/side_menu.tsx
+++ src/components/beta/layout/side_menu.tsx
@@ -101,7 +101,7 @@ const SideMenu = () => {
   return (
     <div className="bg-surface-neutral-main-background">
       {isSideMenuOpen ? (
-        <section className="relative flex h-screen w-max flex-none flex-col gap-24px bg-surface-neutral-surface-lv2 px-12px py-32px shadow-SideMenu">
+        <section className="relative flex h-full w-max flex-none flex-col gap-24px bg-surface-neutral-surface-lv2 px-12px py-32px shadow-SideMenu">
           {/* Side Menu Icon */}
           <div>
             <button type="button" onClick={toggleSideMenu} className="p-10px">
