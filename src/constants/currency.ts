5eea42d05ddabe2061b40164a2f125bf14a8b861
Tiny_Murky <murky0830@gmail.com>
2024-10-24 16:10:14 +0800

    invoice entity complete

--- src/constants/currency.ts
+++ src/constants/currency.ts
@@ -1,6 +1,11 @@
+/**
+ * Info: (20241024 - Murky)
+ * @description All currency allow in ISunFa
+ */
 export enum CurrencyType {
   TWD = 'TWD',
 }
+
 export const OEN_CURRENCY = {
   [CurrencyType.TWD]: CurrencyType.TWD,
 };
