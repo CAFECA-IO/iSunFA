11904665f5146e9530984840e40757bc2c8c6713
Liz <mingreal75@gmail.com>
2024-11-06 11:53:27 +0800

    ToDoListPage

---
+++ src/pages/beta/to_do_list_page.tsx
@@ -0,0 +1,62 @@
+import Head from 'next/head';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { ILocale } from '@/interfaces/locale';
+import { useTranslation } from 'next-i18next';
+import Layout from '@/components/beta/layout/layout';
+import ToDoListPageBody from '@/components/beta/to_do_list_page/to_do_list_page_body';
+import { ISUNFA_ROUTE } from '@/constants/url';
+
+const ToDoListPage = () => {
+  const { t } = useTranslation(['common']);
+
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <title>{t('common:MY_COMPANY_LIST_PAGE.MY_COMPANY_LIST')}</title>
+        <meta
+          name="description"
+          content="iSunFA: Blockchain AI Forensic Accounting and Auditing is where simplicity meets accuracy in the realm of financial investigations."
+        />
+        <meta name="author" content="CAFECA" />
+        <meta name="keywords" content="區塊鏈,人工智慧,會計" />
+
+        <meta property="og:title" content="iSunFA" />
+        <meta
+          property="og:description"
+          content="iSunFA: Blockchain AI Forensic Accounting and Auditing is where simplicity meets accuracy in the realm of financial investigations."
+        />
+      </Head>
+
+      <Layout
+        isDashboard={false}
+        pageTitle={t('common:TO_DO_LIST_PAGE.TO_DO_LIST_TITLE')}
+        goBackUrl={ISUNFA_ROUTE.BETA_DASHBOARD}
+      >
+        <ToDoListPageBody />
+      </Layout>
+    </>
+  );
+};
+
+export const getServerSideProps = async ({ locale }: ILocale) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale as string, [
+        'common',
+        'report_401',
+        'journal',
+        'kyc',
+        'project',
+        'setting',
+        'terms',
+        'salary',
+        'asset',
+      ])),
+    },
+  };
+};
+
+export default ToDoListPage;
