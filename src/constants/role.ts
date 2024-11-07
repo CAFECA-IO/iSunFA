323609ef42759d28b712a674d034411e02d6f18f
Luphia Chang <luphia@mermer.cc>
2024-11-07 19:35:52 +0800

    Update role.ts

--- src/constants/role.ts
+++ src/constants/role.ts
@@ -1,8 +1,8 @@
-// Info: (20241007 - Liz) 角色 id 的概念，用來區分不同的角色，但與後端給的 Role id 不同
+// Info: (20241107 - Liz) 角色 id 的概念，用來區分不同的角色，但與後端給的 Role id 不同
 export enum RoleName {
-  BOOKKEEPER = 'Bookkeeper', // 1006
-  EDUCATIONAL_TRIAL_VERSION = 'Educational Trial Version', // 1008
-  ACCOUNTANT = 'Accountant', // 1007
+  BOOKKEEPER = 'Bookkeeper', // Info: (20241107 - Liz) Bookkeeper = 1006
+  EDUCATIONAL_TRIAL_VERSION = 'Educational Trial Version', // Info: (20241107 - Liz) Educational Trial Version = 1008
+  ACCOUNTANT = 'Accountant', // Info: (20241107 - Liz) Accountant = 1007
 }
 
 export enum CompanyRoleName {
