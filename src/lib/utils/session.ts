feacf21d472c452c6f3310ba0702e611691a4b23
Luphia Chang <luphia@mermer.cc>
2024-11-20 03:02:43 +0800

    use dummy session for development

--- src/lib/utils/session.ts
+++ src/lib/utils/session.ts
@@ -1,4 +1,5 @@
 import { ISessionData } from '@/interfaces/session_data';
+import { NextApiRequest, NextApiResponse } from 'next';
 import nextSession from 'next-session';
 
 const options = {
@@ -9,14 +10,31 @@ const options = {
   },
 };
 
-export const getSession = nextSession<ISessionData>(options);
+// export const getSession = nextSession<ISessionData>(options);
+// Deprecate: (20241119 - Luphia) dummy session for development
+const env = process.env.NODE_ENV;
+const dummyGetSession = async (req: NextApiRequest, res: NextApiResponse) => {
+  const sessionData: ISessionData = {
+    id: 'dummy',
+    cookie: {
+      httpOnly: true,
+      path: '/',
+      secure: true,
+    },
+    userId: 10000121,
+    companyId: 10000001,
+    challenge: 'dummy',
+    roleId: 1006,
+  };
+  return sessionData;
+};
+export const getSession = env === 'development' ? dummyGetSession : nextSession<ISessionData>(options);
 
 export async function setSession(
   session: ISessionData,
   data: { userId?: number; companyId?: number; challenge?: string; roleId?: number }
 ) {
   const { userId, companyId, challenge, roleId } = data;
-
   const updatedSession = session;
 
   if (userId) updatedSession.userId = userId;
