bb4572134b9f9b00e868b161a77402f9ade3bb7d
TzuHanLiang <crocodilestear@gmail.com>
2024-11-22 17:26:27 +0800

    feat: floating upload remain unshow

--- src/components/floating_upload_popup/floating_upload_popup.tsx
+++ src/components/floating_upload_popup/floating_upload_popup.tsx
@@ -75,55 +75,54 @@ const FloatingUploadPopup: React.FC<FloatingUploadPopupProps> = ({
     messageModalVisibilityHandler();
   };
 
-  const popUpBody = (
-    // uploadingFileCount > 0 ? (
-    <div className="dashboardCardShadow fixed bottom-4 right-4 z-50 w-480px overflow-hidden">
-      {/* Info: (20240919 - tzuhan) Header: 顯示標題與收縮/展開按鈕 */}
-      <div className="flex items-center justify-between p-4">
-        <div className="flex-auto flex-col items-center text-center">
-          <div className="flex items-center justify-center space-x-2 text-lg font-semibold">
-            <Image src="/elements/cloud_upload.svg" width={24} height={24} alt="Upload icon" />
-            <div>Upload file</div>
-          </div>
-          {files.length > 0 && (
-            <div className="pb-4 pt-2">
-              <p className="text-sm text-file-uploading-text-disable">
-                {uploadingFileCount > 0
-                  ? `Uploading (${uploadingFileCount}/${files.length})...`
-                  : `Completed (${files.length - uploadingFileCount}/${files.length})`}
-              </p>
+  const popUpBody =
+    uploadingFileCount > 0 ? (
+      <div className="dashboardCardShadow fixed bottom-4 right-4 z-50 w-480px overflow-hidden">
+        {/* Info: (20240919 - tzuhan) Header: 顯示標題與收縮/展開按鈕 */}
+        <div className="flex items-center justify-between p-4">
+          <div className="flex-auto flex-col items-center text-center">
+            <div className="flex items-center justify-center space-x-2 text-lg font-semibold">
+              <Image src="/elements/cloud_upload.svg" width={24} height={24} alt="Upload icon" />
+              <div>Upload file</div>
             </div>
-          )}
+            {files.length > 0 && (
+              <div className="pb-4 pt-2">
+                <p className="text-sm text-file-uploading-text-disable">
+                  {uploadingFileCount > 0
+                    ? `Uploading (${uploadingFileCount}/${files.length})...`
+                    : `Completed (${files.length - uploadingFileCount}/${files.length})`}
+                </p>
+              </div>
+            )}
+          </div>
+          <button
+            type="button"
+            className={`${expanded ? '' : 'rotate-180'}`}
+            onClick={() => setExpanded(!expanded)}
+          >
+            <Image src="/elements/arrow_down.svg" alt="arrow_down" width={20} height={20} />
+          </button>
         </div>
-        <button
-          type="button"
-          className={`${expanded ? '' : 'rotate-180'}`}
-          onClick={() => setExpanded(!expanded)}
-        >
-          <Image src="/elements/arrow_down.svg" alt="arrow_down" width={20} height={20} />
-        </button>
-      </div>
 
-      {/* Info: (20240919 - tzuhan) 當 expanded 為 true 時顯示上傳文件列表 */}
-      {expanded && (
-        <div className="max-h-96 overflow-auto p-4">
-          {files.length > 0 ? (
-            files.map((file, index) => (
-              <UploadCertificateItem
-                key={`uploading-${index + 1}`}
-                file={file}
-                onPauseToggle={() => togglePause(index)}
-                onDelete={() => handleDelete(index)}
-              />
-            ))
-          ) : (
-            <div className="text-center text-gray-500">{t('certificate:UPLOAD.NO_FILE')}</div>
-          )}
-        </div>
-      )}
-    </div>
-  );
-  // ) : null;
+        {/* Info: (20240919 - tzuhan) 當 expanded 為 true 時顯示上傳文件列表 */}
+        {expanded && (
+          <div className="max-h-96 overflow-auto p-4">
+            {files.length > 0 ? (
+              files.map((file, index) => (
+                <UploadCertificateItem
+                  key={`uploading-${index + 1}`}
+                  file={file}
+                  onPauseToggle={() => togglePause(index)}
+                  onDelete={() => handleDelete(index)}
+                />
+              ))
+            ) : (
+              <div className="text-center text-gray-500">{t('certificate:UPLOAD.NO_FILE')}</div>
+            )}
+          </div>
+        )}
+      </div>
+    ) : null;
 
   return popUpBody;
 };
