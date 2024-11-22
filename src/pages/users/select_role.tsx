b3daec36c29ca3fdda96c69322adcdd0280414d4
Liz <mingreal75@gmail.com>
2024-11-22 16:20:26 +0800

    Add return button in Create Role Page

--- src/pages/users/select_role.tsx
+++ src/pages/users/select_role.tsx
@@ -15,6 +15,7 @@ import { RoleName } from '@/constants/role';
 import { IUserRole } from '@/interfaces/user_role';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { IRole } from '@/interfaces/role';
 
 interface UserRoleProps {
   name: string;
@@ -107,25 +108,51 @@ const UserRole = ({
   );
 };
 
+const findUnusedRoles = (systemRoles: IRole[], userRoles: IUserRole[]): IRole[] => {
+  // Info: (20241122 - Liz) 將 userRoles 中的角色 ID 建立為一個 Set
+  const userRoleIds = new Set(userRoles.map((userRole) => userRole.role.id));
+
+  // Info: (20241122 - Liz) 從 systemRoles 中篩選出尚未被 userRoles 使用的角色
+  return systemRoles.filter((role) => !userRoleIds.has(role.id));
+};
+
 const SelectRolePage = () => {
   const { t } = useTranslation(['dashboard']);
-  const { signOut, userAuth, getUserRoleList } = useUserCtx();
+  const { signOut, userAuth, getUserRoleList, getSystemRoleList } = useUserCtx();
   const router = useRouter();
   const [userRoleList, setUserRoleList] = useState<IUserRole[]>([]);
+  const [isAbleToCreateRole, setIsAbleToCreateRole] = useState<boolean>(false);
 
   useEffect(() => {
-    const fetchUserRoleList = async () => {
-      const data = await getUserRoleList();
+    const initializeRolesData = async () => {
+      // Deprecated: (20241122 - Liz)
+      // eslint-disable-next-line no-console
+      console.log('觸發 useEffect, 取得系統角色與使用者角色 (in SelectRolePage)');
 
-      if (data && data.length > 0) {
-        setUserRoleList(data);
-      } else {
-        router.push(ISUNFA_ROUTE.CREATE_ROLE);
+      try {
+        const userRoles = await getUserRoleList();
+        const systemRoles = await getSystemRoleList();
+
+        if (!userRoles || userRoles.length === 0) {
+          router.push(ISUNFA_ROUTE.CREATE_ROLE);
+          return;
+        }
+
+        setUserRoleList(userRoles);
+
+        if (systemRoles && userRoles) {
+          const unusedRoles = findUnusedRoles(systemRoles, userRoles);
+          setIsAbleToCreateRole(unusedRoles.length > 0);
+        }
+      } catch (error) {
+        // Deprecated: (20241122 - Liz)
+        // eslint-disable-next-line no-console
+        console.log('Failed to fetch or compute roles:', error);
       }
     };
 
-    fetchUserRoleList();
-  }, [router]);
+    initializeRolesData();
+  }, [getSystemRoleList, getUserRoleList, router]);
 
   // Info: (20241107 - Liz) 跳轉到建立角色頁面前的 Loading 畫面
   if (userRoleList === null) {
@@ -192,12 +219,14 @@ const SelectRolePage = () => {
             );
           })}
 
-          <Link
-            href={ISUNFA_ROUTE.CREATE_ROLE}
-            className="z-1 rounded-lg bg-surface-neutral-surface-lv1 p-18px text-text-neutral-secondary shadow-Dropshadow_S"
-          >
-            <HiPlus size={64} />
-          </Link>
+          {isAbleToCreateRole && (
+            <Link
+              href={ISUNFA_ROUTE.CREATE_ROLE}
+              className="z-1 rounded-lg bg-surface-neutral-surface-lv1 p-18px text-text-neutral-secondary shadow-Dropshadow_S"
+            >
+              <HiPlus size={64} />
+            </Link>
+          )}
         </section>
       </div>
     </>
