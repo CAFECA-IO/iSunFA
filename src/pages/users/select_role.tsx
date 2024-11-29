1635e225fb66f0255d440d55fa905b01e6da4e4a
TzuHanLiang <crocodilestear@gmail.com>
2024-11-29 14:08:54 +0800

    feat: fix sign out error at select-role page

--- src/pages/users/select_role.tsx
+++ src/pages/users/select_role.tsx
@@ -152,7 +152,7 @@ const SelectRolePage = () => {
     };
 
     initializeRolesData();
-  }, [getSystemRoleList, getUserRoleList, router]);
+  }, [getSystemRoleList, getUserRoleList]);
 
   // Info: (20241107 - Liz) 跳轉到建立角色頁面前的 Loading 畫面
   if (userRoleList === null) {
