89387757ba5a0c3eb4ab32e06c5c30124de3f632
jing12345678910 <onetwo12826@gmail.com>
2024-12-02 22:24:54 +0800

    income_statement_printing_feature

--- src/components/income_statement_report_body/income_statement_list_new.tsx
+++ src/components/income_statement_report_body/income_statement_list_new.tsx
@@ -16,12 +16,21 @@ import { useTranslation } from 'next-i18next';
 import PrintButton from '@/components/button/print_button';
 import DownloadButton from '@/components/button/download_button';
 import { useGlobalCtx } from '@/contexts/global_context';
+import IncomeStatementA4Template from '@/components/income_statement_report_body/income_statement_a4_template';
 
 interface IncomeStatementListProps {
   selectedDateRange: IDatePeriod | null; // Info: (20241024 - Anna) 接收來自上層的日期範圍
+  isPrinting: boolean; // Info: (20241122 - Anna)  從父層傳入的列印狀態
+  printRef: React.RefObject<HTMLDivElement>; // Info: (20241122 - Anna) 從父層傳入的 Ref
+  printFn: () => void; // Info: (20241122 - Anna) 從父層傳入的列印函數
 }
 
-const IncomeStatementList: React.FC<IncomeStatementListProps> = ({ selectedDateRange }) => {
+const IncomeStatementList: React.FC<IncomeStatementListProps> = ({
+  selectedDateRange,
+  isPrinting, // Info: (20241122 - Anna) 使用打印狀態
+  printRef, // Info: (20241122 - Anna) 使用打印範圍 Ref
+  printFn, // Info: (20241122 - Anna) 使用打印函數
+}) => {
   const { t } = useTranslation('reports');
   const { exportVoucherModalVisibilityHandler } = useGlobalCtx();
   // Info: (20241024 - Anna) 接收 selectedDateRange prop
@@ -89,6 +98,21 @@ const IncomeStatementList: React.FC<IncomeStatementListProps> = ({ selectedDateR
     getIncomeStatementReport();
   }, [getIncomeStatementReport, selectedDateRange]);
 
+  useEffect(() => {
+    if (isPrinting && printRef.current) {
+      // Deprecated: (20241130 - Anna) remove eslint-disable
+      // eslint-disable-next-line no-console
+      console.log('income_statement_list 觀察 Printing content:', printRef.current.innerHTML);
+      // Deprecated: (20241130 - Anna) remove eslint-disable
+      // eslint-disable-next-line no-console
+      console.log('IncomeStatementList received isPrinting?', isPrinting);
+    } else {
+      // Deprecated: (20241130 - Anna) remove eslint-disable
+      // eslint-disable-next-line no-console
+      console.log('IncomeStatementList printRef is null');
+    }
+  }, [isPrinting]);
+
   // Info: (20241024 - Anna) 如果未選擇日期範圍，顯示初始提示圖
   if (!hasFetchedOnce && !getReportFinancialIsLoading) {
     return (
@@ -147,14 +171,27 @@ const IncomeStatementList: React.FC<IncomeStatementListProps> = ({ selectedDateR
   const formattedPreToDate = format(preDateTo, 'yyyy-MM-dd');
 
   // Info: (20241101 - Anna) 列印及下載按鈕
-  const displayedSelectArea = (
-    <div className="mb-16px flex items-center justify-end px-px max-md:flex-wrap">
-      <div className="ml-auto flex items-center gap-24px">
-        <DownloadButton onClick={exportVoucherModalVisibilityHandler} disabled={false} />
-        <PrintButton onClick={() => {}} disabled={false} />
+  // const displayedSelectArea = (
+  //   <div className="mb-16px flex items-center justify-end px-px max-md:flex-wrap">
+  //     <div className="ml-auto flex items-center gap-24px">
+  //       <DownloadButton onClick={exportVoucherModalVisibilityHandler} disabled />
+  //       <PrintButton onClick={printFn} disabled={false} />
+  //     </div>
+  //   </div>
+  // );
+  const displayedSelectArea = () => {
+    // Deprecated: (20241130 - Anna) remove eslint-disable
+    // eslint-disable-next-line no-console
+    console.log('[displayedSelectArea] Display Area Rendered');
+    return (
+      <div className="mb-16px flex items-center justify-between px-px max-md:flex-wrap print:hidden">
+        <div className="ml-auto flex items-center gap-24px">
+          <DownloadButton onClick={exportVoucherModalVisibilityHandler} disabled />
+          <PrintButton onClick={printFn} disabled={false} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 
   const ItemSummary = (
     <div id="1" className="relative overflow-hidden">
@@ -539,15 +576,40 @@ const IncomeStatementList: React.FC<IncomeStatementListProps> = ({ selectedDateR
     </div>
   );
 
+  // return (
+  //   <div className="mx-auto w-full origin-top overflow-x-auto">
+  //     <hr className="mb-40px break-before-page" />
+  //     {displayedSelectArea}
+  //     {ItemSummary}
+  //     <hr className="break-before-page" />
+  //     {ItemDetail}
+  //     <hr className="break-before-page" />
+  //     {CostRevRatio}
+  //   </div>
+  // );
   return (
-    <div className="mx-auto w-full origin-top overflow-x-auto">
-      <hr className="mb-40px break-before-page" />
-      {displayedSelectArea}
-      {ItemSummary}
-      <hr className="break-before-page" />
-      {ItemDetail}
-      <hr className="break-before-page" />
-      {CostRevRatio}
+    <div className={`relative mx-auto w-full origin-top overflow-x-auto`}>
+      {displayedSelectArea()}
+      {/* Info: (20241202 - Anna)  渲染打印模板，通過 CSS 隱藏 */}
+      <div ref={printRef} className="hidden print:block">
+        <IncomeStatementA4Template
+          reportFinancial={reportFinancial}
+          curDateFrom={formattedCurFromDate}
+          curDateTo={formattedCurToDate}
+          preDateFrom={formattedPreFromDate}
+          preDateTo={formattedPreToDate}
+        >
+          {ItemSummary}
+          {ItemDetail}
+          {CostRevRatio}
+        </IncomeStatementA4Template>
+      </div>
+      {/* Info: (20241202 - Anna) 預覽區域 */}
+      <div className="block print:hidden">
+        {ItemSummary}
+        {ItemDetail}
+        {CostRevRatio}
+      </div>
     </div>
   );
 };
