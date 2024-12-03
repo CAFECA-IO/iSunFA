d492ae10b54e42fb02c6c2507b7ce354b2437e5b
Luphia Chang <luphia@mermer.cc>
2024-12-03 20:55:37 +0800

    Update index.ts

---
+++ src/pages/api/v2/company/[companyId]/ledger/export/index.ts
@@ -0,0 +1,5 @@
+/* ToDo: (20241203 - Luphia) Do not commit empty file, try to fill with
+ * 1. What is this file for
+ * 2. What is the development plan
+ * 3. Define the interface for the class or function
+ */
