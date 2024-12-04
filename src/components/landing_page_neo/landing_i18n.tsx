c9e594772c6562be1dce76c142c82dbdedeb9b64
julian <julian890701@gmail.com>
2024-12-04 17:15:41 +0800

    Feat: landing page navbar

---
+++ src/components/landing_page_neo/landing_i18n.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import useOuterClick from '@/lib/hooks/use_outer_click';
+
+const LandingI18n: React.FC = () => {
+  return (
+    <div className="relative">
+      <div className="flex h-44px items-center rounded-sm bg-landing-page-white/30 px-24px text-landing-page-white shadow-landing-nav">
+        <p>Language</p>
+      </div>
+    </div>
+  );
+};
+
+export default LandingI18n;
