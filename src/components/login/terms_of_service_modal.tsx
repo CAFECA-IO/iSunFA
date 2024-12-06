2a1b67806fd665203494784e9d183cf880deae8d
Liz <mingreal75@gmail.com>
2024-12-06 17:02:06 +0800

    UI: checkbox

--- src/components/login/terms_of_service_modal.tsx
+++ src/components/login/terms_of_service_modal.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { RxCross2 } from 'react-icons/rx';
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
@@ -7,6 +6,7 @@ import { Hash } from '@/constants/hash';
 import { ISUNFA_ROUTE } from '@/constants/url';
 import { Button } from '@/components/button/button';
 import TermsOfService from '@/components/login/terms_of_service';
+import { useState } from 'react';
 
 interface TermsOfServiceModalProps {
   isModalVisible: boolean;
@@ -20,6 +20,8 @@ const TermsOfServiceModal = ({
   const { t } = useTranslation('terms');
   const { handleUserAgree, signOut } = useUserCtx();
   const router = useRouter();
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = () => setIsChecked((prev) => !prev);
 
   const onAgree = async () => {
     const success = await handleUserAgree(Hash.HASH_FOR_TERMS_OF_SERVICE);
@@ -33,10 +35,10 @@ const TermsOfServiceModal = ({
     signOut();
   };
 
-  const displayModal = isModalVisible ? (
+  return isModalVisible ? (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-      <div className="absolute max-h-80vh w-full max-w-xl rounded-xs bg-surface-neutral-surface-lv1 p-4 pt-0 shadow-lg">
-        <div className="my-4 mb-8 flex items-center justify-between">
+      <div className="absolute max-h-100vh w-full max-w-800px rounded-xs bg-surface-neutral-surface-lv1 p-16px pt-0 shadow-lg">
+        <div className="my-16px mb-32px flex items-center justify-between">
           <h2 className="text-lg font-semibold text-text-neutral-primary">
             {t('terms:MODAL.PLEASE_READ_AND_AGREE_THE_FIRST_TIME_YOU_LOGIN')}
           </h2>
@@ -48,11 +50,29 @@ const TermsOfServiceModal = ({
             <RxCross2 size={20} />
           </button>
         </div>
-        <hr className="absolute left-0 top-60px w-full max-w-xl border-divider-stroke-lv-4" />
-        <div className="m-4 h-50vh overflow-y-auto rounded-xs border border-surface-brand-secondary bg-surface-neutral-main-background lg:p-4">
-          <TermsOfService />
+
+        <hr className="absolute left-0 top-0 mt-60px w-full border-divider-stroke-lv-4" />
+
+        <div className="mx-40px mb-20px mt-60px max-h-60vh overflow-hidden rounded-sm border border-surface-brand-secondary bg-surface-neutral-main-background">
+          <div className="h-full max-h-60vh overflow-y-auto p-40px">
+            <TermsOfService />
+          </div>
         </div>
-        <div className="mx-4 flex justify-end space-x-4">
+
+        <label htmlFor="termsCheckbox" className="mx-40px flex cursor-pointer items-center gap-8px">
+          <input
+            id="termsCheckbox"
+            type="checkbox"
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+            className="h-10px w-10px accent-checkbox-surface-selected"
+          />
+          <span className="text-checkbox-text-primary">
+            {t('terms:MODAL.I_AGREE_WITH_THE_TERM_AFTER_READING_IT_CAREFULLY')}
+          </span>
+        </label>
+
+        <div className="mx-16px mb-12px flex justify-end gap-16px">
           <Button
             type="button"
             onClick={onCancel}
@@ -63,17 +83,17 @@ const TermsOfServiceModal = ({
           </Button>
           <Button
             type="button"
+            disabled={!isChecked}
             onClick={onAgree}
             variant="tertiary"
             className="px-4 py-2 text-sm lg:text-lg"
           >
-            {t('terms:MODAL.AGREE_TO_OUR_TERMS_OF_SERVICE')}
+            {t('terms:MODAL.CONFIRM')}
           </Button>
         </div>
       </div>
     </div>
   ) : null;
-  return displayModal;
 };
 
 export default TermsOfServiceModal;
