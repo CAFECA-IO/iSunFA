7d590a5c1b6975a81b9ab7ce8960d05a26b972e3
Liz <mingreal75@gmail.com>
2024-12-12 17:26:57 +0800

    fn: upload company avatar

--- src/components/beta/my_company_list_page/upload_company_avatar_modal.tsx
+++ src/components/beta/my_company_list_page/upload_company_avatar_modal.tsx
@@ -1,14 +1,19 @@
-import { ICompanyAndRole } from '@/interfaces/company';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 import { IoCloseOutline } from 'react-icons/io5';
 import { useTranslation } from 'react-i18next';
+import { ICompany, ICompanyAndRole } from '@/interfaces/company';
+import { IFileUIBeta } from '@/interfaces/file';
+import { UploadType } from '@/constants/file';
+import { APIName } from '@/constants/api_connection';
+import APIHandler from '@/lib/utils/api_handler';
 import UploadArea from '@/components/upload_area/upload_area';
+import { useUserCtx } from '@/contexts/user_context';
 
 interface UploadCompanyAvatarModalProps {
   companyToUploadAvatar: ICompanyAndRole;
   isModalOpen: boolean;
   setCompanyToUploadAvatar: Dispatch<SetStateAction<ICompanyAndRole | undefined>>;
-  setRefreshKey?: React.Dispatch<React.SetStateAction<number>>;
+  setRefreshKey?: Dispatch<SetStateAction<number>>;
 }
 
 const UploadCompanyAvatarModal = ({
@@ -18,36 +23,77 @@ const UploadCompanyAvatarModal = ({
   setRefreshKey,
 }: UploadCompanyAvatarModalProps) => {
   const { t } = useTranslation(['company']);
+  const { selectedCompany, selectCompany } = useUserCtx();
+  const [isLoading, setIsLoading] = useState(false);
+  const { trigger: uploadFileAPI } = APIHandler<IFileUIBeta>(APIName.FILE_UPLOAD);
+  const { trigger: uploadCompanyAvatarAPI } = APIHandler<ICompany>(APIName.COMPANY_PUT_ICON);
 
-  // Deprecated: (20241210 - Liz)
-  // eslint-disable-next-line no-console
-  console.log('companyToUploadAvatar:', companyToUploadAvatar, setRefreshKey);
-
-  const closeUploadCompanyAvatarModal = () => {
+  const closeUploadCompanyAvatarModal = useCallback(() => {
     setCompanyToUploadAvatar(undefined);
-  };
+  }, [setCompanyToUploadAvatar]);
 
-  const handleUpload = async (file: File) => {
-    // Deprecated: (20241210 - Liz)
-    // eslint-disable-next-line no-console
-    console.log('handleUpload file:', file);
+  const handleUpload = useCallback(
+    async (file: File) => {
+      if (isLoading) return;
+      setIsLoading(true);
 
-    // Info: (20241210 - Liz) 打 API 更新公司圖片 (暫時亂寫 等api完成再補)
-    // try {
-    //   const data = await updateCompanyAvatar(companyToUploadAvatar.company.id, file);
-    //   if (data) {
-    //     setRefreshKey((prev) => prev + 1);
-    //   } else {
-    //     // Deprecated: (20241210 - Liz)
-    //     // eslint-disable-next-line no-console
-    //     console.log('更新公司圖片失敗');
-    //   }
-    // } catch (error) {
-    //   // Deprecated: (20241210 - Liz)
-    //   // eslint-disable-next-line no-console
-    //   console.error('UploadCompanyAvatarModal handleUpload error:', error);
-    // }
-  };
+      try {
+        // Info: (20241212 - Liz) 打 API 上傳檔案
+        const formData = new FormData();
+        formData.append('file', file);
+        const { success: uploadFileSuccess, data: fileMeta } = await uploadFileAPI({
+          query: {
+            type: UploadType.COMPANY,
+            targetId: String(companyToUploadAvatar.company.id),
+          },
+          body: formData,
+        });
+
+        if (!uploadFileSuccess || !fileMeta) {
+          // Deprecated: (20241212 - Liz)
+          // eslint-disable-next-line no-console
+          console.error('Failed to upload file:', file.name);
+          return;
+        }
+
+        // Info: (20241212 - Liz) 打 API 更新公司頭像
+        const { success, error } = await uploadCompanyAvatarAPI({
+          params: { companyId: companyToUploadAvatar.company.id },
+          body: { fileId: fileMeta.id },
+        });
+
+        if (!success) {
+          // Deprecated: (20241212 - Liz)
+          // eslint-disable-next-line no-console
+          console.error('Failed to update company avatar! error message:', error?.message);
+          return;
+        }
+
+        closeUploadCompanyAvatarModal();
+        if (setRefreshKey) setRefreshKey((prev) => prev + 1); // Info: (20241212 - Liz) This is a workaround to refresh the company list after creating a new company
+
+        const isChangingSelectedCompany = selectedCompany?.id === companyToUploadAvatar.company.id;
+
+        // Info: (20241212 - Liz) 如果是改變已選擇的公司，就打 API 再重新選擇一次以更新公司頭像
+        if (isChangingSelectedCompany) {
+          selectCompany(companyToUploadAvatar.company.id);
+        }
+      } catch (error) {
+        // Deprecated: (20241212 - Liz)
+        // eslint-disable-next-line no-console
+        console.error('Failed to upload file:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [
+      closeUploadCompanyAvatarModal,
+      companyToUploadAvatar.company.id,
+      isLoading,
+      selectedCompany?.id,
+      setRefreshKey,
+    ]
+  );
 
   return isModalOpen ? (
     <main className="fixed inset-0 z-10 flex items-center justify-center bg-black/50">
