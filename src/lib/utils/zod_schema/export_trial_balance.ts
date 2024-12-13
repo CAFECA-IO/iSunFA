fd02a9a467477802a162a5eb7a2c26b457c29782
Luphia Chang <luphia@mermer.cc>
2024-12-13 18:36:13 +0800

    Update export_trial_balance.ts

--- src/lib/utils/zod_schema/export_trial_balance.ts
+++ src/lib/utils/zod_schema/export_trial_balance.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 import { nullSchema, zodStringToNumber } from '@/lib/utils/zod_schema/common';
 
-// 查詢參數驗證器
+// Info: (20241213 - Shirley) 查詢參數驗證器
 const IExportTrialBalancePostQueryValidator = z.object({
   companyId: zodStringToNumber,
 });
 
-// 排序欄位驗證器
+// Info: (20241213 - Shirley) 排序欄位驗證器
 const sortFieldSchema = z.enum([
   'beginningCreditAmount',
   'beginningDebitAmount',
@@ -16,13 +16,13 @@ const sortFieldSchema = z.enum([
   'endingDebitAmount',
 ]);
 
-// 排序物件驗證器
+// Info: (20241213 - Shirley) 排序物件驗證器
 const sortSchema = z.object({
   by: sortFieldSchema,
   order: z.enum(['asc', 'desc']),
 });
 
-// 匯出欄位驗證器
+// Info: (20241213 - Shirley) 匯出欄位驗證器
 export const exportTrialBalanceFieldsSchema = z.enum([
   'accountingTitle',
   'beginningCreditAmount',
@@ -33,20 +33,20 @@ export const exportTrialBalanceFieldsSchema = z.enum([
   'endingDebitAmount',
 ]);
 
-// 過濾條件驗證器
+// Info: (20241213 - Shirley) 過濾條件驗證器
 const exportTrialBalanceFiltersSchema = z.object({
   startDate: z.number(),
   endDate: z.number(),
 });
 
-// 匯出選項驗證器
+// Info: (20241213 - Shirley) 匯出選項驗證器
 const exportTrialBalanceOptionsSchema = z.object({
   language: z.enum(['zh-TW', 'en-US']).optional().default('zh-TW'),
   timezone: z.string().optional().default('+0800'),
   fields: z.array(exportTrialBalanceFieldsSchema).optional().default([]),
 });
 
-// Post body schema
+// Info: (20241213 - Shirley) Post body schema
 const IExportTrialBalancePostBodySchema = z.object({
   fileType: z.literal('csv'),
   filters: exportTrialBalanceFiltersSchema,
@@ -54,7 +54,7 @@ const IExportTrialBalancePostBodySchema = z.object({
   options: exportTrialBalanceOptionsSchema.optional(),
 });
 
-// 匯出 schema
+// Info: (20241213 - Shirley) 匯出 schema
 export const exportTrialBalancePostSchema = {
   input: {
     querySchema: IExportTrialBalancePostQueryValidator,
