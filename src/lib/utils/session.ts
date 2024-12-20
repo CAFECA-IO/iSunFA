f8bde950eb394f2b770dd832eb9e9c38a125102d
jing12345678910 <onetwo12826@gmail.com>
2024-12-20 18:39:42 +0800

    restore session

--- src/lib/utils/session.ts
+++ src/lib/utils/session.ts
@@ -1,5 +1,4 @@
 import { ISessionData } from '@/interfaces/session_data';
-import { NextApiRequest, NextApiResponse } from 'next';
 import nextSession from 'next-session';
 
 const options = {
@@ -9,41 +8,25 @@ const options = {
     secure: true,
   },
 };
-// export const getSession = nextSession<ISessionData>(options);
-// Deprecate: (20241119 - Luphia) dummy session for development
-const env = process.env.NODE_ENV;
-// Deprecate: (20241119 - Luphia) dummy session for development
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const dummyGetSession = async (req: NextApiRequest, res: NextApiResponse) => {
-  const sessionData: ISessionData = {
-    id: 'dummy',
-    cookie: {
-      httpOnly: true,
-      path: '/',
-      secure: true,
-    },
-    userId: 10000006, // Info: (20241112 - Anna)
-    companyId: 10000951, // Info: (20241112 - Anna)
-    challenge: 'dummy',
-    roleId: 1006,
-  };
-  return sessionData;
-};
-// Deprecate: (20241119 - Luphia) dummy session for development
-export const getSession =
-  env === 'development' ? dummyGetSession : nextSession<ISessionData>(options);
+
+export const getSession = nextSession<ISessionData>(options);
+
 export async function setSession(
   session: ISessionData,
   data: { userId?: number; companyId?: number; challenge?: string; roleId?: number }
 ) {
   const { userId, companyId, challenge, roleId } = data;
+
   const updatedSession = session;
+
   if (userId) updatedSession.userId = userId;
   if (companyId) updatedSession.companyId = companyId;
   if (challenge) updatedSession.challenge = challenge;
   if (roleId) updatedSession.roleId = roleId;
+
   return updatedSession;
 }
+
 export async function destroySession(session: ISessionData) {
   session.destroy();
 }
