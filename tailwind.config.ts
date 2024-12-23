144a7416113a72491e6ed98ec824966fc8b4378e
TzuHanLiang <crocodilestear@gmail.com>
2024-12-23 16:34:30 +0800

    feat: update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1229,6 +1229,8 @@ module.exports = {
 
         '46vh': '46vh',
         '60vh': '60vh',
+        '70vh': '70vh',
+        '75vh': '75vh',
         '80vh': '80vh',
         '90vh': '90vh',
         '95vh': '95vh',
