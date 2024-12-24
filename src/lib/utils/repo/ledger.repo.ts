7f786cc4b8f9d545f87a28682a082f442a291837
arealclimber <luffulgence@gmail.com>
2024-12-24 15:56:18 +0800

    refactor: 💡 use pure function to calculate ledger instead of
    using account book utils
    
    ✅ Closes: #3721 #3490

--- src/lib/utils/repo/ledger.repo.ts
+++ src/lib/utils/repo/ledger.repo.ts
@@ -9,11 +9,6 @@ import { LabelType } from '@/constants/ledger';
 import { ledgerListSchema } from '@/lib/utils/zod_schema/ledger';
 import { z } from 'zod';
 import { DefaultValue } from '@/constants/default_value';
-import {
-  filterLedgerByAccountNo,
-  filterLedgerItemsByLabelType,
-  getLedgerFromAccountBook,
-} from '@/lib/utils/ledger';
 import { formatPaginatedLedger } from '@/lib/utils/formatter/ledger.formatter';
 
 type ListLedgerParams = z.infer<typeof ledgerListSchema.input.querySchema>;
@@ -39,9 +34,9 @@ export async function listLedger(params: ListLedgerParams): Promise<ILedgerPaylo
       throw new Error(STATUS_MESSAGE.INVALID_INPUT_PARAMETER);
     }
 
-    // Info: (20241112 - Shirley) 1. 取得會計設定的貨幣別
+    // 1. 取得會計設定的貨幣別
     const accountingSettingData = await prisma.accountingSetting.findFirst({
-      where: { id: companyId },
+      where: { companyId },
     });
 
     let currencyAlias = 'TWD';
@@ -49,48 +44,115 @@ export async function listLedger(params: ListLedgerParams): Promise<ILedgerPaylo
       currencyAlias = accountingSettingData.currency || 'TWD';
     }
 
-    const ledgerJSON = await getLedgerFromAccountBook(companyId, startDate, endDate);
-    const voucherIds = ledgerJSON.map((item) => item.voucherId);
-    // Info: (20241203 - Shirley) use voucherIds to get voucher data in voucher table
-    const vouchers = await prisma.voucher.findMany({
+    // 2. 取得範圍內的LineItem
+    const lineItems = await prisma.lineItem.findMany({
       where: {
-        id: { in: voucherIds },
+        voucher: {
+          companyId,
+          createdAt: {
+            gte: startDate,
+            lte: endDate,
+          },
+          deletedAt: null,
+        },
+        deletedAt: null,
       },
-      select: {
-        id: true,
-        date: true,
-        type: true,
-        no: true,
+      include: {
+        voucher: {
+          select: {
+            id: true,
+            date: true,
+            type: true,
+            no: true,
+          },
+        },
+        account: {
+          select: {
+            code: true,
+            name: true,
+            parentId: true,
+          },
+        },
+      },
+      orderBy: {
+        voucher: {
+          date: 'asc',
+        },
       },
     });
 
-    const filteredLedgerByAccount = await filterLedgerByAccountNo(
-      ledgerJSON,
-      startAccountNo,
-      endAccountNo
-    );
+    // 3. 篩選科目範圍
+    let filteredLineItems = lineItems;
+    if (startAccountNo || endAccountNo) {
+      filteredLineItems = lineItems.filter((item) => {
+        const no = item.account.code;
+        if (startAccountNo && endAccountNo) {
+          return no >= startAccountNo && no <= endAccountNo;
+        } else if (startAccountNo) {
+          return no >= startAccountNo;
+        } else if (endAccountNo) {
+          return no <= endAccountNo;
+        }
+        return true;
+      });
+    }
 
-    const combineAndTrimData: ILedgerItem[] = filteredLedgerByAccount.map((ledger) => {
-      const { description: particulars, deletedAt, amount, debit, ...rest } = ledger;
-      const voucherData = vouchers.find((voucher) => voucher.id === ledger.voucherId);
-      const voucherDate = voucherData?.date ?? 0;
-      const voucherNumber = voucherData?.no ?? '';
-      const voucherType =
-        EVENT_TYPE_TO_VOUCHER_TYPE_MAP[voucherData?.type as EventType] || voucherData?.type;
-      return {
-        ...rest,
-        particulars,
-        voucherDate,
-        voucherType,
-        voucherNumber,
-      };
-    });
-    const filteredLedgerByLabelType = await filterLedgerItemsByLabelType(
-      combineAndTrimData,
-      labelType
-    );
+    // 4. 根據 labelType 篩選
+    if (labelType !== LabelType.ALL) {
+      filteredLineItems = filteredLineItems.filter((item) => {
+        if (labelType === LabelType.GENERAL) {
+          return !item.account.code.includes('-');
+        } else if (labelType === LabelType.DETAILED) {
+          return item.account.code.includes('-');
+        }
+        return true;
+      });
+    }
 
-    const sumUpData = filteredLedgerByLabelType.reduce(
+    // 5. 為每一個科目維護累加的balance
+    const accountBalances: { [key: string]: number } = {};
+    const processedLineItems: ILedgerItem[] = filteredLineItems
+      .sort((a, b) => {
+        // 先依照 account.code 排序
+        const codeCompare = a.account.code.localeCompare(b.account.code);
+        // 如果 account.code 相同，則依照 voucher date 排序
+        if (codeCompare === 0) {
+          return a.voucher.date - b.voucher.date;
+        }
+        return codeCompare;
+      })
+      // .sort((a, b) => a.account.code.localeCompare(b.account.code))
+      .map((item) => {
+        const accountKey = item.account.code;
+        if (!accountBalances[accountKey]) {
+          accountBalances[accountKey] = 0;
+        }
+        const debit = item.debit ? item.amount : 0;
+        const credit = !item.debit ? item.amount : 0;
+        const balanceChange = item.debit ? item.amount : -item.amount;
+        accountBalances[accountKey] += balanceChange;
+
+        return {
+          id: item.id,
+          accountId: item.accountId,
+          voucherId: item.voucherId,
+          voucherDate: item.voucher.date,
+          no: item.account.code,
+          accountingTitle: item.account.name,
+          voucherNumber: item.voucher.no,
+          voucherType:
+            EVENT_TYPE_TO_VOUCHER_TYPE_MAP[item.voucher.type as EventType] || item.voucher.type,
+          particulars: item.description,
+          debitAmount: debit,
+          creditAmount: credit,
+          balance: accountBalances[accountKey],
+          createdAt: item.createdAt,
+          updatedAt: item.updatedAt,
+        };
+      });
+
+    // 6. 計算總額
+    const sumUpData: ILedgerTotal = processedLineItems.reduce(
       (acc: ILedgerTotal, item: ILedgerItem) => {
         acc.totalDebitAmount += item.debitAmount;
         acc.totalCreditAmount += item.creditAmount;
@@ -104,7 +166,8 @@ export async function listLedger(params: ListLedgerParams): Promise<ILedgerPaylo
       }
     );
 
-    const paginatedLedger = formatPaginatedLedger(filteredLedgerByLabelType, pageNumber, pageSize);
+    // 7. 分頁處理
+    const paginatedLedger = formatPaginatedLedger(processedLineItems, pageNumber, pageSize);
 
     ledgerPayload = {
       currencyAlias,
@@ -121,20 +184,3 @@ export async function listLedger(params: ListLedgerParams): Promise<ILedgerPaylo
 
   return ledgerPayload;
 }
-
-export const findVouchersByVoucherIds = async (
-  voucherIds: number[]
-): Promise<{ id: number; date: number; no: string; type: string }[]> => {
-  const vouchers = await prisma.voucher.findMany({
-    where: {
-      id: { in: voucherIds },
-    },
-    select: {
-      id: true,
-      date: true,
-      no: true,
-      type: true,
-    },
-  });
-  return vouchers;
-};
