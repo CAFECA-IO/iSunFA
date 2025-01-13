e4d38075c3698ce3b4417e72bc7d158179767b24
jing12345678910 <onetwo12826@gmail.com>
2025-01-13 16:30:23 +0800

    Modify the accounting account display on the income statement

--- src/components/voucher/voucher_list_page_body.tsx
+++ src/components/voucher/voucher_list_page_body.tsx
@@ -72,7 +72,11 @@ const VoucherListPageBody: React.FC = () => {
   const voucherTabs = Object.values(VoucherTabs).map((value) =>
     t(`journal:VOUCHER.${value.toUpperCase()}_TAB`)
   );
-  const voucherTypeList = ['All', ...Object.keys(EventType).map((key) => key.toLowerCase())];
+
+  const voucherTypeList = [
+    'All',
+    ...Object.keys(EventType).map((key) => `${key.toLowerCase()}`),
+  ];
 
   const params = { companyId: selectedCompany?.id ?? FREE_COMPANY_ID };
 
