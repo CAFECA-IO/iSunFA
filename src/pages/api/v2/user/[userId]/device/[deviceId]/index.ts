ce189d8be0b355a24cddc31b44647badd0096afd
Luphia Chang <luphia@cafeca.io>
2025-01-13 15:08:09 +0800

    add annotation

--- src/pages/api/v2/user/[userId]/device/[deviceId]/index.ts
+++ src/pages/api/v2/user/[userId]/device/[deviceId]/index.ts
@@ -4,11 +4,15 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession, kickDevice } from '@/lib/utils/session';
 import { IResponseData } from '@/interfaces/response_data';
 
-/* Info: (20250111 - Luphia) 踢除指定登入裝置
+/* Info: (20250111 - Luphia) 列出用戶所有登入裝置
  * 1. 取得 Session 資訊
  * 2. 檢驗是否具備操作權限
- * 3. 踢除指定 Session
- * 4. 回傳操作結果
+ * 3. 檢查用戶 input (使用 zod)
+ * 4. 具體實作的商業邏輯：根據 deviceId 踢除指定 Device
+ * 5. 紀錄 user action log
+ * 6. 整理例外狀況，紀錄重大異常
+ * 7. 格式化 API 回傳資料
+ * 8. 回傳操作結果
  */
 const handleDeleteRequest = async (req: NextApiRequest) => {
   const targetDeviceId = req.query.deviceId as string;
