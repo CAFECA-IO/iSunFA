f0dc5acc382f3a1ed016cf4ddcd4d53a8c888f2c
julian <julian890701@gmail.com>
2025-02-17 17:34:40 +0800

    develop create team modal

--- src/components/beta/my_account_page/my_account_page_body.tsx
+++ src/components/beta/my_account_page/my_account_page_body.tsx
@@ -1,14 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { FaPlus } from 'react-icons/fa6';
+import { useTranslation } from 'next-i18next';
+import { useUserCtx } from '@/contexts/user_context';
 import { IPaginatedData } from '@/interfaces/pagination';
 import { ILoginDevice } from '@/interfaces/login_device';
+import UserInfo from '@/components/user_settings/user_info';
+import TeamList from '@/components/beta/my_account_page/team_list';
+import CreateTeamModal from '@/components/beta/my_account_page/create_team_modal';
+import { Button } from '@/components/button/button';
 
-interface IMyAccountPageBodyProps {
+interface MyAccountPageBodyProps {
   loginDevices: IPaginatedData<ILoginDevice[]> | null;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const MyAccountPageBody: React.FC<IMyAccountPageBodyProps> = ({ loginDevices }) => {
-  return <div></div>;
+const MyAccountPageBody: React.FC<MyAccountPageBodyProps> = ({ loginDevices }) => {
+  const { t } = useTranslation(['common']);
+  const { userAuth } = useUserCtx();
+
+  const [isCreateTeamModalOpen, setIsCreateTeamModalOpen] = useState(false);
+
+  const loginDevice = loginDevices?.data[0]?.userAgent ?? '';
+  const loginIP = loginDevices?.data[0]?.ipAddress ?? '';
+
+  const toggleCreateTeamModal = () => {
+    setIsCreateTeamModalOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="flex flex-col gap-40px">
+      {/* Info: (20250217 - Julian) 使用者資訊 */}
+      <div className="flex items-center gap-4">
+        <hr className="block flex-1 border-divider-stroke-lv-4 md:hidden" />
+        <div className="flex items-center gap-2 text-sm font-medium text-divider-text-lv-1">
+          <Image src="/icons/user-identifier-card.svg" width={16} height={16} alt="info_icon" />
+          <p>{t('User information')}</p>
+        </div>
+        <hr className="flex-1 border-divider-stroke-lv-4" />
+      </div>
+
+      <UserInfo
+        userId={userAuth?.id ?? 1}
+        username={userAuth?.name ?? ''}
+        email={userAuth?.email ?? ''}
+        loginDevice={loginDevice}
+        loginIP={loginIP}
+        imageId={userAuth?.imageId ?? ''}
+        loginDevices={loginDevices}
+      />
+
+      {/* Info: (20250217 - Julian) 我的團隊 */}
+      <div className="flex items-center gap-4">
+        <hr className="block flex-1 border-divider-stroke-lv-4 md:hidden" />
+        <div className="flex items-center gap-2 text-sm font-medium text-divider-text-lv-1">
+          <Image src="/icons/building.svg" width={16} height={16} alt="info_icon" />
+          <p>{t('My team')}</p>
+        </div>
+        <hr className="flex-1 border-divider-stroke-lv-4" />
+      </div>
+
+      {/* Info: (20250217 - Julian) 團隊列表 */}
+      <div className="flex flex-col gap-lv-4">
+        <Button
+          type="button"
+          variant="tertiary"
+          className="ml-auto"
+          onClick={toggleCreateTeamModal}
+        >
+          <FaPlus /> Add Team
+        </Button>
+
+        <TeamList />
+      </div>
+
+      {/* Info: (20250217 - Julian) 新增團隊視窗 */}
+      {isCreateTeamModalOpen && <CreateTeamModal modalVisibilityHandler={toggleCreateTeamModal} />}
+    </div>
+  );
 };
 
 export default MyAccountPageBody;
--- src/constants/team.ts
+++ src/constants/team.ts
@@ -13,7 +13,8 @@ export const FAKE_TEAM_LIST: ITeam[] = [
       editable: true,
     },
     profile: {
-      value: 'Profile Team A',
+      value:
+        'https://static-cdn.jtvnw.net/jtv_user_pictures/44ed4bd3-5f4f-47e6-b89b-90de65386dbb-profile_banner-480.png',
       editable: true,
     },
     planType: {
@@ -38,7 +39,7 @@ export const FAKE_TEAM_LIST: ITeam[] = [
       editable: true,
     },
     profile: {
-      value: 'Profile Team B',
+      value: 'https://i.pinimg.com/236x/cc/48/41/cc4841ce4212d47ca59c5ec0e63f53bc.jpg',
       editable: true,
     },
     planType: {
@@ -49,6 +50,31 @@ export const FAKE_TEAM_LIST: ITeam[] = [
     totalAccountBooks: 2,
     bankAccount: {
       value: '12345678',
+      editable: true,
+    },
+  },
+  {
+    id: '3',
+    name: {
+      value: 'Team C',
+      editable: true,
+    },
+    about: {
+      value: 'About Team C',
+      editable: true,
+    },
+    profile: {
+      value: 'https://i.pinimg.com/736x/ea/78/01/ea78014cd14a79714042729c661bd46c.jpg',
+      editable: true,
+    },
+    planType: {
+      value: TPlanType.BEGINNER,
+      editable: true,
+    },
+    totalMembers: 1,
+    totalAccountBooks: 1,
+    bankAccount: {
+      value: '87654321',
       editable: true,
     },
   },
