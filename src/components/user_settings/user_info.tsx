90c0f1931adc2c3423b663b0c1bee98fbcf4e032
julian <julian890701@gmail.com>
2025-02-17 17:13:54 +0800

    enhance user info style

--- src/components/user_settings/user_info.tsx
+++ src/components/user_settings/user_info.tsx
@@ -52,7 +52,7 @@ const UserInfo: React.FC<UserInfoProps> = ({
   };
 
   return (
-    <div className="bg-brand-gradient flex items-center gap-lv-7 rounded-md p-4 shadow-normal_setting_brand">
+    <div className="bg-brand-gradient flex items-center gap-lv-7 rounded-md border border-stroke-brand-primary px-40px py-16px">
       {isIPModalOpen && (
         <IPModal userId={userId} toggleModal={toggleIPModal} pageData={loginDevices} />
       )}
