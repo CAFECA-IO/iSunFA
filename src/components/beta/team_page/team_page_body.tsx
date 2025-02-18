875b7878cc7e405bb7f8a3451d2041fa81435e6a
Liz <mingreal75@gmail.com>
2025-02-18 16:57:05 +0800

    TeamPageBody UI

--- src/components/beta/team_page/team_page_body.tsx
+++ src/components/beta/team_page/team_page_body.tsx
@@ -1,18 +1,34 @@
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
-import { PiCopySimple } from 'react-icons/pi';
+import { TbCopyCheckFilled, TbCopy } from 'react-icons/tb';
 import { ITeam } from '@/interfaces/team';
+import { useTranslation } from 'next-i18next';
 
 interface TeamPageBodyProps {
   team: ITeam;
 }
 
 const TeamPageBody = ({ team }: TeamPageBodyProps) => {
-  // Deprecated: (20250218 - Liz)
-  // eslint-disable-next-line no-console
-  console.log('team', team);
+  const { t } = useTranslation(['team']);
+  const [copied, setCopied] = useState<boolean>(false);
 
-  // ToDo: (20250218 - Liz)
-  const copyTeamId = () => {};
+  const copyTeamId = async () => {
+    try {
+      await navigator.clipboard.writeText(team.id);
+      setCopied(true);
+    } catch (error) {
+      // Deprecated: (20250218 - Liz)
+      // eslint-disable-next-line no-console
+      console.error('Failed to copy:', error);
+    }
+  };
+
+  // Info: (20250218 - Liz) 使用 useEffect 清理 setTimeout 的 timer 以避免 memory leak
+  useEffect(() => {
+    if (!copied) return undefined;
+    const timer = setTimeout(() => setCopied(false), 2000);
+    return () => clearTimeout(timer);
+  }, [copied]);
 
   return (
     <main className="flex flex-col gap-40px">
@@ -21,8 +37,9 @@ const TeamPageBody = ({ team }: TeamPageBodyProps) => {
         <h1 className="text-44px font-bold text-text-neutral-primary">{team.name.value}</h1>
         <div className="flex items-center text-text-neutral-tertiary">
           <span className="text-xl font-bold leading-8">#{team.id}</span>
-          <button type="button" onClick={copyTeamId} className="p-10px">
-            <PiCopySimple size={16} />
+          <button type="button" onClick={copyTeamId} className="flex items-center p-10px">
+            {copied ? <TbCopyCheckFilled size={16} /> : <TbCopy size={16} />}
+            {copied && <span className="text-sm">{t('team:TEAM_PAGE.COPIED')}!</span>}
           </button>
         </div>
       </section>
