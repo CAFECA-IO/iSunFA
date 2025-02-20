c2cb161e566b26ba61f11a4c966858589c12698c
Liz <mingreal75@gmail.com>
2025-02-20 14:17:37 +0800

    update TeamPageButtons

--- src/components/beta/team_page/team_page_buttons.tsx
+++ src/components/beta/team_page/team_page_buttons.tsx
@@ -1,8 +1,9 @@
 import { Button } from '@/components/button/button';
 import { FiUser, FiInfo, FiEdit } from 'react-icons/fi';
+import { IoMdLogOut } from 'react-icons/io';
 import Link from 'next/link';
 import { ISUNFA_ROUTE } from '@/constants/url';
-import { ITeam } from '@/interfaces/team';
+import { ITeam, TeamRole } from '@/interfaces/team';
 import { useTranslation } from 'next-i18next';
 
 interface TeamPageButtonsProps {
@@ -11,6 +12,10 @@ interface TeamPageButtonsProps {
 
 const TeamPageButtons = ({ team }: TeamPageButtonsProps) => {
   const { t } = useTranslation(['team']);
+  const { role } = team;
+  const isOwner = role === TeamRole.OWNER;
+  const isEditor = role === TeamRole.EDITOR;
+  const isViewer = role === TeamRole.VIEWER;
 
   return (
     <main className="flex gap-16px">
@@ -26,12 +31,23 @@ const TeamPageButtons = ({ team }: TeamPageButtonsProps) => {
         <FiInfo size={16} />
       </Link>
 
-      <button
-        type="button"
-        className="rounded-xs bg-button-surface-strong-secondary p-10px text-button-text-invert hover:bg-button-surface-strong-secondary-hover disabled:bg-button-surface-strong-disable disabled:text-button-text-disable"
-      >
-        <FiEdit size={16} />
-      </button>
+      {isOwner && (
+        <button
+          type="button"
+          className="rounded-xs bg-button-surface-strong-secondary p-10px text-button-text-invert hover:bg-button-surface-strong-secondary-hover disabled:bg-button-surface-strong-disable disabled:text-button-text-disable"
+        >
+          <FiEdit size={16} />
+        </button>
+      )}
+
+      {(isEditor || isViewer) && (
+        <button
+          type="button"
+          className="rounded-xs bg-button-surface-strong-secondary p-10px text-button-text-invert hover:bg-button-surface-strong-secondary-hover disabled:bg-button-surface-strong-disable disabled:text-button-text-disable"
+        >
+          <IoMdLogOut size={16} />
+        </button>
+      )}
     </main>
   );
 };
