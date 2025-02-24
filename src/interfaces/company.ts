f1c2bb125de81247b9a97995bab6e5d648ce6910
Liz <mingreal75@gmail.com>
2025-02-24 17:27:30 +0800

    update interface

--- src/interfaces/company.ts
+++ src/interfaces/company.ts
@@ -1,5 +1,6 @@
 import { WORK_TAG } from '@/constants/company';
 import { IRole } from '@/interfaces/role';
+import { ITeam } from '@/interfaces/team';
 
 // ToDo: (20250219 - Liz) ICompany 將被改名成 IAccountBook (因為公司已經改名成帳本)
 // Info: (20250219 - Liz) 新增一個屬性 isPrivate ，用來判斷是否為私人帳本，這只有 owner 可以設定。如果是公開帳本，帳本才可以被其他使用者看到
@@ -11,7 +12,7 @@ export interface ICompany {
   startDate: number;
   createdAt: number;
   updatedAt: number;
-  isPrivate?: boolean;
+  isPrivate?: boolean; // ToDo: (20250224 - Liz) 等後端 API 調整後就改為必填
 }
 
 export interface ICompanyDetail extends ICompany {
@@ -24,15 +25,18 @@ export interface ICompanyAndRoleDetail {
   role: IRole;
 }
 
-// ToDo: (20250219 - Liz) ICompanyAndRole 將被改名成 IAccountBookAndRole
-// 這個資料格式目前只有使用 company.id 和 tag
-// order 跟 role 都沒有用到
-// tag 是每個使用者都可以設定自己專屬的工作標籤，不是帳本本身的屬性
+// ToDo: (20250224 - Liz) ICompanyAndRole 將被改名成 IAccountBookForUser
+// tag, order, role 都是使用者的個人化設定 (個人專屬的工作標籤、排序、個人角色)
+// 目前 order 跟 role 尚未用到
 export interface ICompanyAndRole {
-  company: ICompany;
+  company: ICompany; // IAccountBook
   tag: WORK_TAG;
   order: number;
   role: IRole;
+}
+
+export interface IAccountBookForUserWithTeam extends ICompanyAndRole {
+  team: ITeam;
 }
 
 export interface ICompanyTaxIdAndName {
