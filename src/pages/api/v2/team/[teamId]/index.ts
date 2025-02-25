185590596a4865b0ab4485e32cfcd851540d6aba
Tzuhan <tzuhan@CAFECAdeMacBook-Air.local>
2025-02-25 16:52:31 +0800

    feat: update mock api

--- src/pages/api/v2/team/[teamId]/index.ts
+++ src/pages/api/v2/team/[teamId]/index.ts
@@ -2,55 +2,63 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { STATUS_MESSAGE } from '@/constants/status_code';
 import { formatApiResponse } from '@/lib/utils/common';
 import { ITeam } from '@/interfaces/team';
-import { withRequestValidation } from '@/lib/utils/middleware';
 import { APIName } from '@/constants/api_connection';
-import { IResponseData } from '@/interfaces/response_data';
-import { IHandleRequest } from '@/interfaces/handleRequest';
+import { checkRequestData, checkSessionUser, checkUserAuthorization } from '@/lib/utils/middleware';
+import { getSession } from '@/lib/utils/session';
 import { FAKE_TEAM_LIST } from '@/constants/team';
+import loggerBack from '@/lib/utils/logger_back';
+import { HTTP_STATUS } from '@/constants/http';
 
-const handleGetRequest: IHandleRequest<APIName.GET_TEAM_BY_ID, ITeam | null> = async ({
-  query,
-}) => {
+const handleGetRequest = async (req: NextApiRequest) => {
+  const session = await getSession(req);
+  const { userId } = session;
   let statusMessage: string = STATUS_MESSAGE.BAD_REQUEST;
   let payload: ITeam | null = null;
+  const isLogin = await checkSessionUser(session, APIName.GET_TEAM_BY_ID, req);
+  if (!isLogin) {
+    throw new Error(STATUS_MESSAGE.UNAUTHORIZED_ACCESS);
+  }
+  const isAuth = await checkUserAuthorization(APIName.GET_TEAM_BY_ID, req, session);
+  if (!isAuth) {
+    throw new Error(STATUS_MESSAGE.FORBIDDEN);
+  }
+  const { query } = checkRequestData(APIName.GET_TEAM_BY_ID, req, session);
+  if (query === null) {
+    throw new Error(STATUS_MESSAGE.INVALID_INPUT_PARAMETER);
+  }
 
   statusMessage = STATUS_MESSAGE.SUCCESS;
   const { teamId } = query;
+
+  loggerBack.info(
+    `user ${userId} get Team by teamId: ${teamId} with query: ${JSON.stringify(query)}`
+  );
+
   const team = FAKE_TEAM_LIST.find((t) => t.id === teamId);
   payload = team || null;
 
-  return { statusMessage, payload };
+  const result = formatApiResponse(statusMessage, payload);
+  return result;
 };
 
-const methodHandlers: {
-  [key: string]: (
-    req: NextApiRequest,
-    res: NextApiResponse
-  ) => Promise<{ statusMessage: string; payload: ITeam | null }>;
-} = {
-  GET: (req) => withRequestValidation(APIName.GET_TEAM_BY_ID, req, handleGetRequest),
-};
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<IResponseData<ITeam | null>>
-) {
-  let statusMessage: string = STATUS_MESSAGE.BAD_REQUEST;
-  let payload: ITeam | null = null;
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const method = req.method || 'GET';
+  let httpCode = HTTP_STATUS.INTERNAL_SERVER_ERROR;
+  let result;
 
   try {
-    const handleRequest = methodHandlers[req.method || ''];
-    if (handleRequest) {
-      ({ statusMessage, payload } = await handleRequest(req, res));
-    } else {
-      statusMessage = STATUS_MESSAGE.METHOD_NOT_ALLOWED;
+    switch (method) {
+      case 'GET':
+      default:
+        ({ httpCode, result } = await handleGetRequest(req));
     }
-  } catch (_error) {
-    const error = _error as Error;
-    statusMessage = error.message;
-    payload = null;
-  } finally {
-    const { httpCode, result } = formatApiResponse<ITeam | null>(statusMessage, payload);
-    res.status(httpCode).json(result);
+  } catch (error) {
+    const err = error as Error;
+    ({ httpCode, result } = formatApiResponse<null>(
+      STATUS_MESSAGE[err.message as keyof typeof STATUS_MESSAGE],
+      null
+    ));
   }
+
+  res.status(httpCode).json(result);
 }
--- src/pages/api/v2/team/index.ts
+++ src/pages/api/v2/team/index.ts
@@ -1,62 +1,63 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { STATUS_MESSAGE } from '@/constants/status_code';
 import { formatApiResponse } from '@/lib/utils/common';
-import { withRequestValidation } from '@/lib/utils/middleware';
+import { checkRequestData, checkSessionUser, checkUserAuthorization } from '@/lib/utils/middleware';
 import { APIName } from '@/constants/api_connection';
-import { IResponseData } from '@/interfaces/response_data';
-import { IHandleRequest } from '@/interfaces/handleRequest';
 import { IPaginatedData, IPaginatedOptions } from '@/interfaces/pagination';
 import { toPaginatedData } from '@/lib/utils/formatter/pagination';
 import { ITeam } from '@/interfaces/team';
 import { FAKE_TEAM_LIST } from '@/constants/team';
+import { getSession } from '@/lib/utils/session';
+import { HTTP_STATUS } from '@/constants/http';
+import loggerBack from '@/lib/utils/logger_back';
 
-const handleGetRequest: IHandleRequest<
-  APIName.LIST_TEAM,
-  IPaginatedData<ITeam[]> | null
-> = async () => {
+const handleGetRequest = async (req: NextApiRequest) => {
+  const session = await getSession(req);
+  const { userId } = session;
   let statusMessage: string = STATUS_MESSAGE.BAD_REQUEST;
   let payload: IPaginatedData<ITeam[]> | null = null;
+  const isLogin = await checkSessionUser(session, APIName.LIST_TEAM, req);
+  if (!isLogin) {
+    throw new Error(STATUS_MESSAGE.UNAUTHORIZED_ACCESS);
+  }
+  const isAuth = await checkUserAuthorization(APIName.LIST_TEAM, req, session);
+  if (!isAuth) {
+    throw new Error(STATUS_MESSAGE.FORBIDDEN);
+  }
+  const { query } = checkRequestData(APIName.LIST_TEAM, req, session);
+  if (query === null) {
+    throw new Error(STATUS_MESSAGE.INVALID_INPUT_PARAMETER);
+  }
+
+  loggerBack.info(`List Team by userId: ${userId} with query: ${JSON.stringify(query)}`);
 
   statusMessage = STATUS_MESSAGE.SUCCESS;
   const options: IPaginatedOptions<ITeam[]> = {
     data: FAKE_TEAM_LIST.slice(0, 1),
   };
   payload = toPaginatedData(options);
-  return { statusMessage, payload };
+  const result = formatApiResponse(statusMessage, payload);
+  return result;
 };
 
-const methodHandlers: {
-  [key: string]: (
-    req: NextApiRequest,
-    res: NextApiResponse
-  ) => Promise<{ statusMessage: string; payload: IPaginatedData<ITeam[]> | null }>;
-} = {
-  GET: (req) => withRequestValidation(APIName.LIST_TEAM, req, handleGetRequest),
-};
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<IResponseData<IPaginatedData<ITeam[]> | null>>
-) {
-  let statusMessage: string = STATUS_MESSAGE.BAD_REQUEST;
-  let payload: IPaginatedData<ITeam[]> | null = null;
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const method = req.method || 'GET';
+  let httpCode = HTTP_STATUS.INTERNAL_SERVER_ERROR;
+  let result;
 
   try {
-    const handleRequest = methodHandlers[req.method || ''];
-    if (handleRequest) {
-      ({ statusMessage, payload } = await handleRequest(req, res));
-    } else {
-      statusMessage = STATUS_MESSAGE.METHOD_NOT_ALLOWED;
+    switch (method) {
+      case 'GET':
+      default:
+        ({ httpCode, result } = await handleGetRequest(req));
     }
-  } catch (_error) {
-    const error = _error as Error;
-    statusMessage = error.message;
-    payload = null;
-  } finally {
-    const { httpCode, result } = formatApiResponse<IPaginatedData<ITeam[]> | null>(
-      statusMessage,
-      payload
-    );
-    res.status(httpCode).json(result);
+  } catch (error) {
+    const err = error as Error;
+    ({ httpCode, result } = formatApiResponse<null>(
+      STATUS_MESSAGE[err.message as keyof typeof STATUS_MESSAGE],
+      null
+    ));
   }
+
+  res.status(httpCode).json(result);
 }
