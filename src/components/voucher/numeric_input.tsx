2d5a95782c5aa814397e441a46adf57bd9cef6e0
julian <julian890701@gmail.com>
2025-03-06 18:39:55 +0800

    add arrow key function

--- src/components/voucher/numeric_input.tsx
+++ src/components/voucher/numeric_input.tsx
@@ -79,24 +79,65 @@ const NumericInput = ({
     }
   };
 
-  // Info: (20250306 - Julian) 暫存輸入的值
-  let temp = '';
-  // Info: (20250306 - Julian) 處理在中文輸入法填入數字的情況
+  // Info: (20250306 - Julian) 處理在中文輸入法下，填入數字的情況
   const convertInput = (event: React.KeyboardEvent<HTMLInputElement>) => {
     event.preventDefault(); // Info: (20250306 - Julian) 阻止預設事件
+
+    let temp = displayValue; // Info: (20250306 - Julian) 取得目前顯示值
     let code = ''; // Info: (20250306 - Julian) 按鍵 code
+
+    const input = event.currentTarget; // Info: (20250306 - Julian) 取得 input 元件
+    const cursorPos = input.selectionStart ?? displayValue.length; // Info: (20250306 - Julian) 取得當前游標位置
 
     // Info: (20250306 - Julian) 如果按下的是數字鍵
     if (event.code.indexOf('Digit') > -1) {
       code = event.code.slice(-1); // Info: (20250306 - Julian) 取得數字鍵的值
-    }
+      temp = temp.slice(0, cursorPos) + code + temp.slice(cursorPos); // Info: (20250306 - Julian) 插入數字
 
-    // Info: (20250306 - Julian) 如果有取得按鍵值
-    if (code) {
-      // Info: (20250306 - Julian) 將按鍵值加入暫存
-      temp = displayValue + code;
-      // Info: (20250306 - Julian) 更新顯示值
+      // Info: (20250306 - Julian) 變更顯示值
       handleChange({ target: { value: temp } } as React.ChangeEvent<HTMLInputElement>);
+    } else if (event.code === 'Backspace' && cursorPos > 0) {
+      // Info: (20250306 - Julian) 如果按下 backspace
+
+      // Info: (20250306 - Julian) 刪除游標前一個字
+      temp = temp.slice(0, cursorPos - 1) + temp.slice(cursorPos);
+      // Info: (20250306 - Julian) 變更顯示值
+      handleChange({ target: { value: temp } } as React.ChangeEvent<HTMLInputElement>);
+
+      // Info: (20250306 - Julian) 變更游標位置
+      requestAnimationFrame(() => {
+        // Info: (20250306 - Julian) 在 displayValue 歸零前，游標向左移動
+        if (displayValue === '0') {
+          input.setSelectionRange(0, 0);
+        } else input.setSelectionRange(cursorPos - 1, cursorPos - 1);
+      });
+    } else if (event.code === 'Delete' && cursorPos < temp.length) {
+      // Info: (20250306 - Julian) 如果按下 delete
+
+      // Info: (20250306 - Julian) 刪除游標後一個字
+      temp = temp.slice(0, cursorPos) + temp.slice(cursorPos + 1);
+      // Info: (20250306 - Julian) 變更顯示值
+      handleChange({ target: { value: temp } } as React.ChangeEvent<HTMLInputElement>);
+
+      // Info: (20250306 - Julian) 變更游標位置
+      requestAnimationFrame(() => {
+        input.setSelectionRange(cursorPos, cursorPos); // Info: (20250306 - Julian) 游標不動
+      });
+    } else if (event.code === 'ArrowLeft') {
+      // Info: (20250306 - Julian) 如果按下左方向鍵
+      requestAnimationFrame(() => {
+        // Info: (20250306 - Julian) 游標左移
+        input.setSelectionRange(Math.max(cursorPos - 1, 0), Math.max(cursorPos - 1, 0));
+      });
+    } else if (event.code === 'ArrowRight') {
+      // Info: (20250306 - Julian) 如果按下右方向鍵
+      requestAnimationFrame(() => {
+        // Info: (20250306 - Julian) 游標右移
+        input.setSelectionRange(
+          Math.min(cursorPos + 1, temp.length),
+          Math.min(cursorPos + 1, temp.length)
+        );
+      });
     }
   };
 
