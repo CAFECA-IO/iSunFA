41266a677157476cf744028b049812926a28150c
Tzuhan <tzuhan@CAFECAdeMacBook-Air.local>
2025-03-14 16:35:48 +0800

    feat: update DB & update transfer api & add cancel/accept/decline transfer mock api

---
+++ prisma/migrations/10_remove_unique_from_team_to_team/migration.sql
@@ -0,0 +1,23 @@
+-- DropIndex
+DROP INDEX "accountBook_transfer_from_team_id_to_team_id_key";
+
+-- AlterTable
+ALTER TABLE "accountBook_transfer" ALTER COLUMN "created_at" SET DEFAULT EXTRACT(EPOCH FROM NOW())::int,
+ALTER COLUMN "updated_at" SET DEFAULT EXTRACT(EPOCH FROM NOW())::int;
+
+-- AlterTable
+ALTER TABLE "pending_team_member" ALTER COLUMN "created_at" SET DEFAULT EXTRACT(EPOCH FROM NOW())::int;
+
+-- AlterTable
+ALTER TABLE "team" ALTER COLUMN "created_at" SET DEFAULT EXTRACT(EPOCH FROM NOW())::int,
+ALTER COLUMN "updated_at" SET DEFAULT EXTRACT(EPOCH FROM NOW())::int;
+
+-- AlterTable
+ALTER TABLE "team_invoice" ALTER COLUMN "created_at" SET DEFAULT EXTRACT(EPOCH FROM NOW())::int;
+
+-- AlterTable
+ALTER TABLE "team_payment_transaction" ALTER COLUMN "created_at" SET DEFAULT EXTRACT(EPOCH FROM NOW())::int;
+
+-- AlterTable
+ALTER TABLE "team_subscription" ALTER COLUMN "created_at" SET DEFAULT EXTRACT(EPOCH FROM NOW())::int,
+ALTER COLUMN "updated_at" SET DEFAULT EXTRACT(EPOCH FROM NOW())::int;
