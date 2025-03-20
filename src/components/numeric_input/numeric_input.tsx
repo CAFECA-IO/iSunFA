4623f84cc0d6af0f35eab84a202d8b7646fa6b07
jing12345678910 <onetwo12826@gmail.com>
2025-03-20 08:06:36 +0800

    making_accounting_entries_using_decimals

--- src/components/numeric_input/numeric_input.tsx
+++ src/components/numeric_input/numeric_input.tsx
@@ -115,7 +115,7 @@ const NumericInput: React.FC<INumericInputProps> = ({
 
     if (event.code.indexOf('Digit') > -1) {
       // Info: (20250306 - Julian) 如果按下的是數字鍵
-      code = event.code.slice(-1);
+      code = event.code.slice(-1); // Info: (20250306 - Julian) 取得數字鍵的值
       // Info: (20250319 - Anna) 允許輸入小數點，但只能輸入一次
     } else if (event.key === '.' && !displayValue.includes('.')) {
       code = '.';
