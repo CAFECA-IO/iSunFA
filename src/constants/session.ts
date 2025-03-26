387ceda16a8ef66f1251f3449b0145dd8a8fd927
jing12345678910 <onetwo12826@gmail.com>
2025-03-26 13:51:36 +0800

    download_401_report

--- src/constants/session.ts
+++ src/constants/session.ts
@@ -13,11 +13,11 @@ export const SESSION_DEVELOPER: ISessionData = {
   userId: 10000000,
   companyId: 10000000,
   roleId: 1006,
-  teamId: 6, // TODO: (20250324 - Shirley) 改用 teams 來判斷用戶在團隊裡面的權限。
+  teamId: 2, // TODO: (20250324 - Shirley) 改用 teams 來判斷用戶在團隊裡面的權限。
   teamRole: TeamRole.OWNER, // TODO: (20250324 - Shirley) 改用 teams 來判斷用戶在團隊裡面的權限。
   teams: [
     {
-      id: 1, // Info: (20250324 - Shirley) 在 npm run dev 開發環境，可修改 id 為 DB 裡 team table 的 id，之後後端會用 session 來判斷權限
+      id: 6, // Info: (20250324 - Shirley) 在 npm run dev 開發環境，可修改 id 為 DB 裡 team table 的 id，之後後端會用 session 來判斷權限
       role: TeamRole.OWNER,
     },
     {
