fa9350e80cd5a6ccfbe68dbd09dadf5673386622
julian <julian890701@gmail.com>
2025-04-15 16:35:45 +0800

    develop experience bar

--- src/interfaces/experience.ts
+++ src/interfaces/experience.ts
@@ -1,3 +1,23 @@
+// Info: (20250415 - Julian) ------------ Common ------------
+export enum ExperienceType {
+  EDUCATION = 'education',
+  WORK = 'work',
+}
+
+export type IExperienceDate = {
+  year: number;
+  month: number;
+};
+
+export interface IExperienceBar {
+  id: number;
+  mainTitle: string;
+  subTitle: string;
+  start: IExperienceDate;
+  end: IExperienceDate;
+}
+
+// Info: (20250415 - Julian) ------------ Education ------------
 export enum Degree {
   ELEMENTARY = 'Elementary',
   JUNIOR = 'Junior',
@@ -12,12 +32,6 @@ export enum SchoolStatus {
   IN_SCHOOL = 'In_School',
   DROPOUT = 'Dropout',
 }
-
-export type IExperienceDate = {
-  year: number;
-  month: number;
-};
-
 export interface IEducationExperience {
   id: number;
   degree: Degree;
@@ -27,6 +41,41 @@ export interface IEducationExperience {
   end: IExperienceDate;
   status: SchoolStatus;
 }
+
+export const dummyEducationExperience: IEducationExperience[] = [
+  {
+    id: 1,
+    degree: Degree.JUNIOR,
+    schoolName: 'National Taipei University of Technology',
+    department: 'Department of Computer Science and Information Engineering',
+    start: {
+      year: 2015,
+      month: 9,
+    },
+    end: {
+      year: 2018,
+      month: 6,
+    },
+    status: SchoolStatus.GRADUATED,
+  },
+  {
+    id: 2,
+    degree: Degree.BACHELOR,
+    schoolName: 'National Taiwan University',
+    department: 'Computer Science and Information Engineering',
+    start: {
+      year: 2018,
+      month: 9,
+    },
+    end: {
+      year: 2020,
+      month: 6,
+    },
+    status: SchoolStatus.GRADUATED,
+  },
+];
+
+// Info: (20250415 - Julian) ------------ Work ------------
 
 export interface IWorkExperience {
   id: number;
@@ -38,35 +87,44 @@ export interface IWorkExperience {
   leavingReason?: string;
 }
 
-export const dummyEducationExperience: IEducationExperience[] = [
+export const dummyWorkExperience: IWorkExperience[] = [
   {
     id: 1,
-    degree: Degree.JUNIOR,
-    schoolName: 'National Taipei University of Technology',
-    department: 'Department of Computer Science and Information Engineering',
+    companyName: 'Tech Company',
+    position: 'Software Engineer',
     start: {
-      year: 2018,
-      month: 9,
+      year: 2020,
+      month: 1,
     },
     end: {
-      year: 2020,
+      year: 2021,
       month: 6,
     },
-    status: SchoolStatus.GRADUATED,
   },
   {
     id: 2,
-    degree: Degree.BACHELOR,
-    schoolName: 'National Taiwan University',
-    department: 'Computer Science and Information Engineering',
+    companyName: 'Another Tech Company',
+    position: 'Frontend Developer',
     start: {
-      year: 2020,
-      month: 9,
+      year: 2021,
+      month: 7,
     },
     end: {
-      year: 2024,
-      month: 6,
+      year: 2023,
+      month: 12,
     },
-    status: SchoolStatus.GRADUATED,
+  },
+  {
+    id: 3,
+    companyName: 'Web Solutions Inc.',
+    position: 'Full Stack Developer',
+    start: {
+      year: 2023,
+      month: 1,
+    },
+    end: {
+      year: 2023,
+      month: 7,
+    },
   },
 ];
