e34ad683928d26dbe825559ba664d448d00729db
julian <julian890701@gmail.com>
2025-04-21 16:43:37 +0800

    test pay mjml

---
+++ src/constants/email.ts
@@ -0,0 +1,7 @@
+export enum MJML_FILE {
+  FREE = 'src/email/free.mjml',
+  INVITE = 'src/email/invite.mjml',
+  PAY = 'src/email/pay.mjml',
+  PAY_ERROR = 'src/email/pay_error.mjml',
+  SUBSCRIBE = 'src/email/subscribe.mjml',
+}
---
+++ src/interfaces/email.ts
@@ -0,0 +1,13 @@
+export interface IInviteTemplate {
+  inviterName: string;
+  teamName: string;
+  inviteLink: string;
+}
+
+export interface IPayTemplate {
+  planName: string;
+  userName: string;
+  startTime: string;
+  endTime: string;
+  price: string;
+}
