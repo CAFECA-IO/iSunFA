63e3b4c85238d91f256007ea560074f19de332b1
Tzuhan <tzuhan@CAFECAdeMacBook-Air.local>
2025-05-28 19:12:05 +0800

    feat: update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,13 @@
-import { PrismaClient, TeamPlanType } from '@prisma/client';
-import accounts from '@/seed_json/account_new.json';
-import files from '@/seed_json/file.json';
-import lineItems from '@/seed_json/line_item.json';
+import { PrismaClient, Tag, TeamPlanType } from '@prisma/client';
+import accounts from '@/seed_json/account.json';
+import companies from '@/seed_json/company.json';
+import country from '@/seed_json/country.json';
 import teamPlans from '@/seed_json/team_plan.json';
+
 // Info (2024722 - Murky) - Uncomment this line to seed generated reports
 // import generatedReports from '@/seed_json/generated_report.json';
 
 const prisma = new PrismaClient();
-async function createFile() {
-  const data = files.map((f) => {
-    return {
-      ...f,
-      iv: Buffer.from(f.iv, 'base64'),
-    };
-  });
-  await prisma.file.createMany({
-    data,
-  });
-}
 
 async function createAccount() {
   await prisma.account.createMany({
@@ -25,44 +15,19 @@ async function createAccount() {
   });
 }
 
-async function createLineItem(lineItem: {
-  id: number;
-  amount: number;
-  description: string;
-  accountCode: string;
-  debit: boolean;
-  voucherId: number;
-  createdAt: number;
-  updatedAt: number;
-}) {
-  const account = await prisma.account.findFirst({
-    where: {
-      code: lineItem.accountCode,
-    },
-    select: {
-      id: true,
-    },
-  });
-  if (!account) {
-    throw new Error(`Account with code ${lineItem.accountCode} not found`);
-  }
-  await prisma.lineItem.createMany({
-    data: [
-      {
-        id: lineItem.id,
-        amount: lineItem.amount,
-        description: lineItem.description,
-        debit: lineItem.debit,
-        createdAt: lineItem.createdAt,
-        updatedAt: lineItem.updatedAt,
-        accountId: account.id,
-        voucherId: lineItem.voucherId,
-      },
-    ],
+async function createCompany() {
+  await prisma.company.createMany({
+    data: companies.map((company) => ({
+      ...company,
+      tag: company.tag as Tag,
+    })),
   });
 }
-async function createLineItems() {
-  await Promise.all(lineItems.map((lineItem) => createLineItem(lineItem)));
+
+async function createCountry() {
+  await prisma.country.createMany({
+    data: country,
+  });
 }
 
 async function createTeamPlans() {
@@ -116,17 +81,10 @@ async function createTeamPlans() {
 
 async function main() {
   try {
-    await createFile();
-
-    await new Promise((resolve) => {
-      setTimeout(resolve, 3000);
-    });
-    await new Promise((resolve) => {
-      setTimeout(resolve, 3000);
-    });
+    await createCompany();
     await createAccount();
+    await createCountry();
     await createTeamPlans();
-    await createLineItems();
   } finally {
     await prisma.$disconnect();
   }
