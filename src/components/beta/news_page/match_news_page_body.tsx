e66842e5328ab5ae848a8fad52c9d1cb374ad768
julian <julian890701@gmail.com>
2025-06-16 18:07:07 +0800

    add go back button

--- src/components/beta/news_page/match_news_page_body.tsx
+++ src/components/beta/news_page/match_news_page_body.tsx
@@ -7,6 +7,10 @@ import APIHandler from '@/lib/utils/api_handler';
 import { APIName } from '@/constants/api_connection';
 import { INews } from '@/interfaces/news';
 import { useTranslation } from 'next-i18next';
+import { Button } from '@/components/button/button';
+import { TbArrowBackUp } from 'react-icons/tb';
+import { useRouter } from 'next/router';
+import { ISUNFA_ROUTE } from '@/constants/url';
 
 // Info: (20241024 - Liz) 在 Markdown 中，單個 Enter 不會換行，必須在行尾加上兩個空格來觸發換行。使用 remarkBreaks 可以實現直接按 Enter 來換行，不再需要在每行末尾加上兩個空格。
 
@@ -25,8 +29,11 @@ const CustomList = (
 
 const MatchNewsPageBody = ({ newsId }: MatchNewsPageBodyProps) => {
   const { t } = useTranslation(['dashboard']);
+  const router = useRouter();
 
   const [news, setNews] = useState<INews | null>(null);
+
+  const goBack = () => router.push(ISUNFA_ROUTE.LATEST_NEWS_PAGE);
 
   // Info: (20241128 - Liz) 打 API 取得新聞內容
   const { trigger: getNewsByIdAPI } = APIHandler<INews>(APIName.NEWS_GET_BY_ID);
@@ -61,9 +68,15 @@ const MatchNewsPageBody = ({ newsId }: MatchNewsPageBodyProps) => {
   return (
     <main className="flex min-h-full flex-col gap-lv-6 tablet:gap-40px">
       {/* Info: (20250616 - Julian) Mobile title */}
-      <p className="block text-base font-semibold text-text-neutral-secondary tablet:hidden">
-        {t('dashboard:LATEST_NEWS_PAGE.MATCH_NEWS')}
-      </p>
+      <div className="flex items-center gap-lv-2 tablet:hidden">
+        <Button variant="secondaryBorderless" size="defaultSquare" onClick={goBack}>
+          <TbArrowBackUp size={24} />
+        </Button>
+        <p className="text-base font-semibold text-text-neutral-secondary">
+          {t('dashboard:LATEST_NEWS_PAGE.MATCH_NEWS')}
+        </p>
+      </div>
+
       <Image
         src={`${news.imageId}`}
         width={800}
