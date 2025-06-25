066c7b0b90c8e423a9196d55be61a134a3a7b800
Luphia Chang <luphia@cafeca.io>
2025-06-25 15:03:22 +0800

    use constant error message

--- src/lib/utils/ethers/find_hash_cash.ts
+++ src/lib/utils/ethers/find_hash_cash.ts
@@ -1,4 +1,5 @@
 import { keccak256 } from 'ethers';
+import { STATUS_MESSAGE } from '@/constants/status_code';
 import { encodeApplyData } from '@/lib/utils/ethers/rlp';
 
 /**
@@ -26,9 +27,7 @@ export async function findValidHashcash(url: string): Promise<{
   for (let i = 0; i < MAX_ATTEMPTS; i += 1) {
     const hashcash = generateHashcash();
     const timestamp = Math.floor(Date.now() / 1000); // Info: (20250618 - Tzuhan) UNIX 秒數
-
     const encoded = encodeApplyData(url, hashcash, timestamp);
-
     const hash = keccak256(encoded);
 
     if (hash.slice(2, 5) === 'cfc') {
@@ -36,5 +35,5 @@ export async function findValidHashcash(url: string): Promise<{
     }
   }
 
-  throw new Error('找不到符合 cfc prefix 的 hashcash，請增加 MAX_ATTEMPTS 或檢查邏輯');
+  throw new Error(STATUS_MESSAGE.FAILED_TO_FIND_HASH_CASH);
 }
