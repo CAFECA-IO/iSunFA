755047a4b1e79a39dd740be5f745c5e99d49aae5
julian <julian890701@gmail.com>
2025-06-25 12:04:24 +0800

    add simple query

--- src/components/beta/dashboard/pending_task.tsx
+++ src/components/beta/dashboard/pending_task.tsx
@@ -37,7 +37,7 @@ const PendingTask = ({ getTodoList }: PendingTasksProps) => {
       try {
         const { data, success, code } = await getAccountBookListByUserIdAPI({
           params: { userId: userAuth.id },
-          query: { page: 1, pageSize: 999 },
+          query: { page: 1, pageSize: 999, simple: true },
         });
         const accountBookListData = data?.data ?? []; // Info: (20250306 - Liz) 取出帳本清單
 
