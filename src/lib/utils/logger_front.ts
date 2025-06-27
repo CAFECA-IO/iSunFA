de5079ac212c5925d7bc8b8861aba8aac00cde2a
jing12345678910 <onetwo12826@gmail.com>
2025-06-27 17:21:11 +0800

    remove-eslint-disable-comments

--- src/lib/utils/logger_front.ts
+++ src/lib/utils/logger_front.ts
@@ -3,25 +3,25 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 const loggerFront = {
   log: (...args: unknown[]) => {
     if (isDevelopment) {
-      // Info: (20250627 - Anna)
+      // Info: (20250627 - Anna) Debug
       // eslint-disable-next-line no-console
       console.log(...args);
     }
   },
   warn: (...args: unknown[]) => {
     if (isDevelopment) {
-      // Info: (20250627 - Anna)
+      // Info: (20250627 - Anna) Debug
       // eslint-disable-next-line no-console
       console.warn(...args);
     }
   },
   error: (...args: unknown[]) => {
     if (isDevelopment) {
-      // Info: (20250627 - Anna)
+      // Info: (20250627 - Anna) Debug
       // eslint-disable-next-line no-console
       console.error(...args);
     }
-    // Info: (20250627 - Anna): 可以在這裡發送錯誤到錯誤追蹤平台 (Sentry, API...)
+    // Info: (20250627 - Anna) 此處可整合錯誤追蹤平台，例如 Sentry 或自建 API
   },
 };
 
