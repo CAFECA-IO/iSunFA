3a238d0843e3fec770a9cb31041dd7ab122cd1cc
Luphia Chang <luphia@mermer.cc>
2025-08-04 12:16:12 +0800

    Update crypto.ts

--- src/lib/utils/crypto.ts
+++ src/lib/utils/crypto.ts
@@ -135,8 +135,8 @@ export async function decryptData(
       name: ASYMMETRIC_CRYPTO_ALGORITHM,
     },
     privateKey,
-    encryptedData
-  );
+    encryptedData as BufferSource
+  )) as ArrayBuffer;
   return decoder.decode(decryptedData);
 }
 
