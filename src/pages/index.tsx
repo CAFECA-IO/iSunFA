a04963f6d7c6d4f4972891a4ebebb5530290a55d
julian <julian890701@gmail.com>
2025-10-02 17:09:55 +0800

    add meta label

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,11 @@ const LandingPage: React.FC = () => {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta content="企業觀測站" property="og:title" />
+        <meta
+          content="提供上市、上櫃及興櫃公司之重要資訊，涵蓋財務報告、重大訊息與股東會資料，協助投資人快速掌握公司動態與市場趨勢，同時亦揭露未公開發行公司的相關資訊。"
+          property="og:description"
+        />
         <link rel="icon" href="/favicon/favicon.ico" />
         <title>iSunFA</title>
       </Head>
