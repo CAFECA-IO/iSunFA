449705d02fd0019e17e37ee48b07f115be8a3d99
julian <julian890701@gmail.com>
2025-10-14 18:45:42 +0800

    develop invoice item

---
+++ src/components/ai_accounting_assistance/invoice_item.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Image from 'next/image';
+import { FiTrash2 } from 'react-icons/fi';
+import { IoWarningOutline } from 'react-icons/io5';
+
+// ToDo: (20251014 - Julian) During development
+interface IInvoiceItem {
+  id: number;
+  name: string;
+  thumbnail: string;
+  unread: boolean;
+}
+
+// ToDo: (20251014 - Julian) during development
+const InvoiceItem = ({ invoice, isSelected }: { invoice: IInvoiceItem; isSelected: boolean }) => {
+  const { id, name, unread } = invoice;
+  const unfinished = true; // ToDo: (20251014 - Julian) determine if the invoice is unfinished
+
+  const deleteInvoice = () => {
+    // ToDo: (20251014 - Julian) delete invoice function
+    // eslint-disable-next-line no-console
+    console.log(`Delete invoice ${id}`);
+  };
+
+  return (
+    <div
+      className={`${
+        isSelected
+          ? 'border-stroke-brand-primary' // ToDo: (20251014 - Julian) background color
+          : 'border-stroke-neutral-quaternary hover:border-stroke-brand-primary hover:bg-surface-brand-primary-30'
+      } flex items-center gap-8px rounded-xs border p-8px hover:cursor-pointer`}
+    >
+      {/* Info: (20251014 - Julian) Unread icon */}
+      {unread && (
+        <Image src="/icons/unread_indicator.svg" width={10} height={10} alt="unread_indicator" />
+      )}
+      {/* Info: (20251014 - Julian) Thumbnail */}
+      <div className="relative h-48px w-48px shrink-0 rounded-xs border border-stroke-neutral-quaternary bg-surface-neutral-solid-light">
+        <Image src="/images/fake_team_img.svg" fill objectFit="contain" alt="invoice_thumbnail" />
+      </div>
+      {/* Info: (20251014 - Julian) Id */}
+      <div className="flex flex-1 items-center gap-4px">
+        <p className="max-w-60px truncate text-sm font-medium text-text-neutral-primary">{name}</p>
+        {unfinished && <IoWarningOutline size={16} className="shrink-0 text-icon-surface-error" />}
+      </div>
+      {/* Info: (20251014 - Julian) Delete */}
+      <button
+        type="button"
+        onClick={deleteInvoice}
+        className="p-8px text-icon-surface-single-color-primary"
+      >
+        <FiTrash2 size={20} />
+      </button>
+    </div>
+  );
+};
+
+export default InvoiceItem;
--- src/components/ai_accounting_assistance/sidebar.tsx
+++ src/components/ai_accounting_assistance/sidebar.tsx
@@ -1,57 +1,114 @@
 import React from 'react';
-import { FiLayout } from 'react-icons/fi';
+import { FiLayout, FiLogIn } from 'react-icons/fi';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Button } from '@/components/button/button';
 import { ISUNFA_ROUTE } from '@/constants/url';
+import { numberWithCommas } from '@/lib/utils/common';
+import InvoiceItem from '@/components/ai_accounting_assistance/invoice_item';
 
 interface ISidebarProps {
   isOpen: boolean;
   toggleSidebar: () => void;
 }
 
+// ToDo: (20251014 - Julian) During development
+interface IInvoiceItem {
+  id: number;
+  name: string;
+  thumbnail: string;
+  unread: boolean;
+}
+
+const mockInvoiceList: IInvoiceItem[] = [
+  {
+    id: 1,
+    name: 'Invoice 001',
+    thumbnail: '/public/images/fake_avatar.png',
+    unread: false,
+  },
+  {
+    id: 2,
+    name: 'Invoice 002',
+    thumbnail: '/public/images/fake_avatar.png',
+    unread: false,
+  },
+  {
+    id: 3,
+    name: 'Invoice 003',
+    thumbnail: '/public/images/fake_avatar.png',
+    unread: true,
+  },
+];
+
 const AAASidebar: React.FC<ISidebarProps> = ({ isOpen, toggleSidebar }) => {
+  // ToDo: (20251014 - Julian) Replace mock data with real data from backend
+  const invoiceList = mockInvoiceList;
+  const invoiceCount = numberWithCommas(invoiceList.length);
+
+  const loginBtn = (
+    <Link href={ISUNFA_ROUTE.LOGIN}>
+      <Button type="button" size={isOpen ? 'default' : 'defaultSquare'} className="w-full">
+        {isOpen ? 'Log in' : <FiLogIn size={20} />}
+      </Button>
+    </Link>
+  );
+
+  const displayedList =
+    invoiceList.length > 0 ? (
+      <div className="flex flex-col gap-8px">
+        <p className="text-sm font-medium text-text-neutral-tertiary">
+          {invoiceCount} Certificates
+        </p>
+        {/* ToDo: (20251014 - Julian) Develop Filter section */}
+        <div className="flex items-center"></div>
+        {invoiceList.map((invoice) => (
+          <InvoiceItem key={invoice.id} invoice={invoice} isSelected />
+        ))}
+      </div>
+    ) : (
+      <div className="flex flex-col items-center">
+        <p className="text-center text-sm font-medium text-text-neutral-tertiary">
+          No Certificate found Please upload the certificate
+        </p>
+      </div>
+    );
+
+  const displayedContent = isOpen && (
+    <div className="flex flex-col gap-32px">
+      <p className="text-sm font-semibold uppercase text-text-neutral-tertiary">Invoice List</p>
+      {displayedList}
+    </div>
+  );
+
   return (
     <div
       className={`${isOpen ? 'w-250px px-16px' : 'w-70px px-12px'} fixed flex h-screen flex-col gap-32px bg-surface-neutral-surface-lv1 py-16px transition-all duration-200 ease-in-out`}
     >
       {/* Info: (20251014 - Julian) Header */}
-      <div className="flex items-center gap-8px">
-        {isOpen && (
-          <Link href={ISUNFA_ROUTE.DASHBOARD} className="flex flex-1 items-center gap-8px">
-            <Image src="/logo/isunfa_logo_new_icon.svg" width={28} height={28} alt="iSunFA_logo" />
-            <p className="font-semibold text-text-brand-primary-lv2">FinPilot</p>
-          </Link>
-        )}
-        <Button
-          type="button"
-          variant="secondaryBorderless"
-          size="defaultSquare"
-          onClick={toggleSidebar}
-        >
-          <FiLayout size={24} />
-        </Button>
-      </div>
-
-      {/* Info: (20251014 - Julian) Menu Content */}
-      {isOpen && (
-        <div className="flex flex-1 flex-col items-stretch gap-32px">
-          {/* Info: (20251014 - Julian) Login button */}
-          <Link href={ISUNFA_ROUTE.LOGIN}>
-            <Button type="button" className="w-full">
-              Log in
-            </Button>
-          </Link>
-
-          {/* Info: (20251014 - Julian) Invoice List */}
-          <p className="text-sm font-semibold uppercase text-text-neutral-tertiary">Invoice List</p>
-          <div className="flex flex-col items-center">
-            <p className="text-center text-sm font-medium text-text-neutral-tertiary">
-              No Certificate found Please upload the certificate
-            </p>
-          </div>
+      <button type="button" onClick={toggleSidebar} className="group flex items-center gap-8px">
+        <div className="mx-auto shrink-0">
+          <Image src="/logo/isunfa_logo_new_icon.svg" width={28} height={28} alt="iSunFA_logo" />
         </div>
-      )}
+        {isOpen && (
+          <>
+            <p className="flex-1 text-left font-semibold text-text-brand-primary-lv2">FinPilot</p>
+            <FiLayout
+              size={24}
+              className="text-button-text-secondary hover:text-button-text-primary-hover group-hover:text-button-text-primary-hover"
+            />
+          </>
+        )}
+      </button>
+
+      {/* Info: (20251014 - Julian) Body */}
+      <div className="flex flex-1 flex-col items-stretch gap-32px">
+        {/* Info: (20251014 - Julian) Login button */}
+        {loginBtn}
+
+        {/* Info: (20251014 - Julian) Content */}
+        {displayedContent}
+      </div>
 
       {/* Info: (20251014 - Julian) Footer */}
       {isOpen && (
