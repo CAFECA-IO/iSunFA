2c360d08a2f0934d60a1ca9480ad8dc8805f987f
julian <julian890701@gmail.com>
2025-11-11 10:23:29 +0800

    remove dummy notifications

--- src/components/beta/layout/mobile/notification_mobile.tsx
+++ src/components/beta/layout/mobile/notification_mobile.tsx
@@ -10,29 +10,37 @@ interface NotificationMobileProps {
 const NotificationMobile = ({ notifications }: NotificationMobileProps) => {
   const { t } = useTranslation(['dashboard']);
 
+  const isNotifications = notifications.length !== 0;
+
+  const allReadButton = isNotifications && (
+    <button
+      type="button"
+      // ToDo: (20251111 - Julian) all read notification API
+      className="flex items-center gap-4px self-end rounded-xs px-12px py-8px hover:bg-button-surface-soft-secondary-hover disabled:text-button-text-disable"
+    >
+      <FiCheckCircle size={16} className="text-button-text-secondary" />
+      <span className="text-button-text-secondary">{t('dashboard:HEADER.MARK_AS_ALL_READ')}</span>
+    </button>
+  );
+
+  const notificationList = isNotifications ? (
+    <section className="overflow-y-auto">
+      {notifications.map((notification) => (
+        <NotificationItemMobile key={notification.id} notification={notification} />
+      ))}
+    </section>
+  ) : (
+    <div className="flex items-center justify-center p-12px">
+      <span className="text-base font-medium text-text-neutral-tertiary">
+        {t('dashboard:HEADER.NO_NEW_NOTIFICATIONS')}
+      </span>
+    </div>
+  );
+
   return (
     <main className="flex min-h-0 flex-col px-8px py-12px">
-      <button
-        type="button"
-        className="flex items-center gap-4px self-end rounded-xs px-12px py-8px hover:bg-button-surface-soft-secondary-hover disabled:text-button-text-disable"
-      >
-        <FiCheckCircle size={16} className="text-button-text-secondary" />
-        <span className="text-button-text-secondary">{t('dashboard:HEADER.MARK_AS_ALL_READ')}</span>
-      </button>
-
-      <section className="overflow-y-auto">
-        {notifications.map((notification) => (
-          <NotificationItemMobile key={notification.id} notification={notification} />
-        ))}
-      </section>
-
-      {notifications.length === 0 && (
-        <div className="flex items-center justify-center p-12px">
-          <span className="text-base font-medium text-text-neutral-tertiary">
-            {t('dashboard:HEADER.NO_NEW_NOTIFICATIONS')}
-          </span>
-        </div>
-      )}
+      {allReadButton}
+      {notificationList}
     </main>
   );
 };
