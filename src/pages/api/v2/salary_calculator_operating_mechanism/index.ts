562ab4150689e075f470ed68a3caa5d1086b053f
julian <julian890701@gmail.com>
2025-11-13 15:16:40 +0800

    remove unnecessary property

--- src/pages/api/v2/salary_calculator_operating_mechanism/index.ts
+++ src/pages/api/v2/salary_calculator_operating_mechanism/index.ts
@@ -52,7 +52,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       version,
       lastUpdated,
       content,
-      recentFile,
     };
 
     res.status(200).json(responseData);
