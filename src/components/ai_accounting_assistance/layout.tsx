d36a6c828b37a7b244aef10cd50e6cee9e031bd1
julian <julian890701@gmail.com>
2025-11-14 14:35:51 +0800

    develop chat input

--- src/components/ai_accounting_assistance/layout.tsx
+++ src/components/ai_accounting_assistance/layout.tsx
@@ -11,13 +11,13 @@ const AAALayout: React.FC<ILayoutProps> = ({ children }) => {
   const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
 
   return (
-    <main className="min-h-screen overflow-x-hidden bg-surface-neutral-main-background">
+    <main className="min-h-screen w-screen overflow-x-hidden bg-surface-neutral-main-background">
       {/* Info: (20251014 - Julian) Left: Sidebar */}
       <AAASidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
 
       {/* Info: (20251014 - Julian) Right: Main Area */}
       <div
-        className={`${isSidebarOpen ? 'ml-250px' : 'ml-70px'} flex h-screen w-full grow flex-col justify-center gap-140px bg-aaa bg-cover bg-no-repeat px-40px py-120px transition-all duration-200 ease-in-out`}
+        className={`${isSidebarOpen ? 'ml-250px' : 'ml-70px'} flex h-screen grow flex-col justify-center gap-140px bg-aaa bg-cover bg-no-repeat px-40px py-120px transition-all duration-200 ease-in-out`}
       >
         {children}
       </div>
