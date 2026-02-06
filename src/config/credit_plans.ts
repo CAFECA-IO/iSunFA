6c90de8f06166becd7b53912fe13403c1a8eb34f
Tzuhan <crocodilestear@gmail.com>
2026-02-06 17:39:18 +0800

    feat: update

---
+++ src/config/credit_plans.ts
@@ -0,0 +1,69 @@
+export const CREDIT_PLANS = [
+    {
+        id: 'tier1',
+        credits: 150,
+        price: {
+            twd: 150,
+            usd: 5,
+            cny: 35,
+            jpy: 700,
+            krw: 7000,
+        },
+    },
+    {
+        id: 'tier2',
+        credits: 700,
+        price: {
+            twd: 600,
+            usd: 20,
+            cny: 140,
+            jpy: 2800,
+            krw: 28000,
+        },
+        popular: true,
+    },
+    {
+        id: 'tier3',
+        credits: 2000,
+        price: {
+            twd: 1500,
+            usd: 50,
+            cny: 350,
+            jpy: 7000,
+            krw: 70000,
+        },
+    },
+    {
+        id: 'tier4',
+        credits: 4500,
+        price: {
+            twd: 3000,
+            usd: 100,
+            cny: 700,
+            jpy: 14000,
+            krw: 140000,
+        },
+    },
+    {
+        id: 'tier5',
+        credits: 25000,
+        price: {
+            twd: 15000,
+            usd: 500,
+            cny: 3500,
+            jpy: 70000,
+            krw: 700000,
+        },
+    },
+    {
+        id: 'tier6',
+        credits: 60000,
+        price: {
+            twd: 30000,
+            usd: 1000,
+            cny: 7000,
+            jpy: 140000,
+            krw: 1400000,
+        },
+    },
+];
